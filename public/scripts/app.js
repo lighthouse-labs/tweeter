d1c85076a2cb6a1a53fe0a3588408fcacdd9a7a3
Nat York <natyork@gmail.com>
2016-10-19 15:35:53 +0000

    cleaned up app.js and index.html

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,66 +1,3 @@
-/*
- * Client-side JS logic goes here
- * jQuery is already loaded
- * Reminder: Use (and do all your DOM work in) jQuery's document ready function
- */
-
-
-function createTweetElement(td) {
-  var $tweet = $("<article>").addClass("tweet");
-  var $header = $("<header>");
-  var $avatar = $("<img>").addClass("avatar").attr("src", td.user.avatars.regular);
-  var $user = $("<h2>").addClass("user").text(td.user.name);
-  var $handle = $("<span>").addClass("handle").text(td.user.handle);
-  var $content = $("<div>").text(td.content.text);
-  var days_elapsed = Math.floor((Date.now() - td.created_at)/(1000*60*60*24));
-  var $footer = $("<footer>");
-  var $createdAt = $("<span>").text(days_elapsed + " days ago");
-  var $iconHeart = $("<i>").attr({"class": "fa fa-heart", "aria-hidden": "true"});
-  var $iconRetweet = $("<i>").attr({"class": "fa fa-retweet", "aria-hidden": "true"});
-  var $iconFlag = $("<i>").attr({"class": "fa fa-font-awesome", "aria-hidden": "true"});
-
-  $header = $header.append($avatar).append($user).append($handle);
-  $footer = $footer.append($createdAt).append($iconHeart).append($iconRetweet).append($iconFlag);
-
-  $tweet = $tweet.append($header).append($content).append($footer);
-
-  return $tweet
-}
-
-// function renderTweets() {
-
-// }
-
-// console.log(require('../../server/lib/tweets.json'));
-
-$(document).ready(function(){
-// Test / driver code (temporary). Eventually will get this from the server.
-var tweetData = {
-  "user": {
-    "name": "Newton",
-    "avatars": {
-      "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
-      "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
-      "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
-    },
-    "handle": "@SirIsaac"
-  },
-  "content": {
-    "text": "If I have seen further it is by standing on the shoulders of giants"
-  },
-  "created_at": 1461116232227
-}
-
-var $tweet = createTweetElement(tweetData);
-
-// Test / driver code (temporary)
-console.log($tweet);
-
-$('#tweet-container').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
-
-
-
-});
 // Fake data taken from tweets.json
 var data = [
   {
@@ -109,15 +46,36 @@ var data = [
   }
 ];
 
+
+function createTweetElement(td) {
+  var $tweet = $("<article>").addClass("tweet");
+  var $header = $("<header>");
+  var $avatar = $("<img>").addClass("avatar").attr("src", td.user.avatars.regular);
+  var $user = $("<h2>").addClass("user").text(td.user.name);
+  var $handle = $("<span>").addClass("handle").text(td.user.handle);
+  var $content = $("<div>").text(td.content.text);
+  var days_elapsed = Math.floor((Date.now() - td.created_at)/(1000*60*60*24));
+  var $footer = $("<footer>");
+  var $createdAt = $("<span>").text(days_elapsed + " days ago");
+  var $iconHeart = $("<i>").attr({"class": "fa fa-heart", "aria-hidden": "true"});
+  var $iconRetweet = $("<i>").attr({"class": "fa fa-retweet", "aria-hidden": "true"});
+  var $iconFlag = $("<i>").attr({"class": "fa fa-font-awesome", "aria-hidden": "true"});
+
+  $header = $header.append($avatar).append($user).append($handle);
+  $footer = $footer.append($createdAt).append($iconHeart).append($iconRetweet).append($iconFlag);
+
+  $tweet = $tweet.append($header).append($content).append($footer);
+
+  return $tweet
+}
+
+
 function renderTweets(arr) {
   for (i in arr){
     tweetObj = arr[i];
     var $tweet = createTweetElement(tweetObj);
     $('#tweet-container').append($tweet);
   }
-  // loops through tweets
-    // calls createTweetElement for each tweet
-    // takes return value and appends it to the tweets container
 }
 
 
