6beb4f976294614a600243183298d12cd3187715
Brett Goss <brettgoss@live.com>
2016-10-18 20:57:37 -0700

    created and styled tweet template

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -3,14 +3,13 @@ $(document).ready(function() {
   console.log('ready!');
   document.getElementById("text").addEventListener("input", function(e) {
 
-    var max = 140;
+    var max = 141;
     var current = this.value.length;
     $(this).parent().find(".counter").html(max - current);
     if (current > max) {
-      console.log(max - current);
       $(this).parent().find(".counter").css({'color': 'red'});
     } else {
-      console.log(max - current);
+      $(this).parent().find(".counter").css({'color': ''});
     }
-  })
+  });
 });
