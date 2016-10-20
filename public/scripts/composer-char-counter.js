65be5322b6d9e2ef1ec91c630677c339bd5e0fdd
Linley Faulkner <linleyjfaulkner@gmail.com>
2016-10-20 01:18:49 +0000

    adding counter

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,37 @@
+$(function() {
+
+console.log("ready")
+//document ready callback function inside
+// which all our DOM-related logic
+//Using jQuery and an appropriate selector,
+// register an event handler to the textarea 
+//element for the form inside of the .new-tweet section
+//http://api.jquery.com/category/events/event-handler-attachment/
+
+// $(".new-tweet").click(function(){
+// alert("hello world");
+
+// });
+
+$("#tweet-field").on("keyup", function() {
+
+var maxLength = 140;
+var length = this.value.length;
+var countLength = maxLength - length;
+console.log(countLength);
+
+$(this).parent().find(".counter").html(countLength);
+
+
+});
+
+});
+
+
+
+
+
+
+
+
+
