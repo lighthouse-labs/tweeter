717d9d88096356d5f6404c0ec92e4cf9bb09bbf8
Nat York <natyork@gmail.com>
2016-10-24 16:05:47 +0000

    database working and refactored

--- server/lib/db.js
+++ server/lib/db.js
@@ -3,62 +3,43 @@
 const MongoClient = require("mongodb").MongoClient;
 const MONGODB_URI = "mongodb://127.0.0.1:27017/tweeter";
 
+let collection;
+
 const dbMethods = {
 
   saveTweet: (data) => {
-
-    MongoClient.connect(MONGODB_URI, (err, db) => {
-
-      if (err) {
-        console.log('Could not connect! Unexpected error. Details below.');
-        throw err;
-      }
-
-      console.log('Connected to the database!');
-      let collection = db.collection("tweets");
-
-      console.log('Retrieving documents for the "tweets" collection...');
-
-      collection.insertOne(data);
-
-    });
-
+    collection.insertOne(data);
   },
 
   getTweets: (cb) => {
-
-
-    MongoClient.connect(MONGODB_URI, (err, db) => {
-
-      if (err) {
-        console.log('Could not connect! Unexpected error. Details below.');
-        throw err;
-      }
-
-      console.log('Connected to the database!');
-      let collection = db.collection("tweets");
-
-      console.log('Retrieving documents for the "tweets" collection...');
-      collection.find().toArray((err, results) => {
+    collection.find().toArray((err, results) => {
       let output = results.sort(function(a, b) { return a.created_at - b.created_at });
-
-        console.log('results: ', results);
-        db.close();
-        console.log('Disconnecting from Mongo!');
         cb(output);
-      });
-
     });
   }
-
 }
+
 
 module.exports = {
 //onConnect is an arbitrary name for a callback function
   connect: (onConnect) => {
 
-    onConnect(dbMethods);
+    MongoClient.connect(MONGODB_URI, (err, db) => {
 
+      if (err) {
+        console.log('Could not connect! Unexpected error. Details below.');
+        throw err;
+      }
+
+      collection = db.collection("tweets");
+
+      onConnect(dbMethods);
+
+      //db close when program terminates
+      // const disconnect = () => { db.close() }
+      // process.on('SIGINT', disconnect); // on Ctrl-C
+      // process.on('SIGTERM', disconnect);
+
+    })
   }
-
 }
