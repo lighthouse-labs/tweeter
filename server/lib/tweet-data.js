71a6c00c8ebef6d7ff946bc7a79ca8ca56cbc8a0
vaz <v@tryptid.com>
2016-11-16 11:47:51 -0800

    Rename simulateAsync to simulateDelay

--- server/lib/tweet-data.js
+++ server/lib/tweet-data.js
@@ -1,18 +1,18 @@
 "use strict";
 
-const simulateAsync = require("./util/simulate-async");
+const simulateDelay = require("./util/simulate-delay");
 
 module.exports = function(db) {
   return {
     saveTweet: function(newTweet, callback) {
-      simulateAsync(() => {
+      simulateDelay(() => {
         db.tweets.push(newTweet);
         callback(null, true);
       });
     },
 
     getTweets: function(callback) {
-      simulateAsync(() => {
+      simulateDelay(() => {
         const sortNewestFirst = (a, b) => a.created_at - b.created_at;
         callback(null, db.tweets.sort(sortNewestFirst));
       });
