7f0dc880d567fd1764547de74cee5f9c7935d2c5
vaz <v@tryptid.com>
2016-11-16 15:22:29 -0800

    Rename resources/ to data-files/

--- server/lib/in-memory-db.js
+++ server/lib/in-memory-db.js
@@ -3,7 +3,7 @@
 // Requiring a JSON file automatically parses it and returns the data. These
 // are just example tweets to make it less tedious to style the app initially.
 const db = {
-  tweets: require("../resources/initial-tweets")
+  tweets: require("../data-files/initial-tweets")
 }
 
 module.exports = db;
