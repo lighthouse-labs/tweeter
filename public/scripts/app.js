6708709cea2d2c56ee322cb2932d93712e04ae8b
opt1 <opt1@dc604.ca>
2017-02-21 14:10:47 -0800

    Finished W3D2

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -58,18 +58,27 @@ function findDateDifference(daysAgo){
   return Math.ceil(timeDiff / (1000 * 3600 * 24));
 }
 
+function escapeIt(str){
+  return str
+    .replace(/&/g, "&amp;")
+    .replace(/</g, "&lt;")
+    .replace(/>/g, "&gt;")
+    .replace(/"/g, "&quot;")
+    .replace(/'/g, "&#039;");
+}
+
 function createTweetElement(tweetData){
   let diffDays = findDateDifference(tweetData.created_at);
   
   let article = '<article class="hover tweet">';
   let header = '<header class="hover header-tweet">'
-              + '<img class="hover header-pic" src="' + tweetData.user.avatars.small + '">'
-              + '<h3 class="hover header-text">' + tweetData.user.name + '</h3>'
-              + '<p class="hover header-user">' + tweetData.user.handle + '</p>'
+              + '<img class="hover header-pic" src="' + escapeIt(tweetData.user.avatars.small) + '">'
+              + '<h3 class="hover header-text">' + escapeIt(tweetData.user.name) + '</h3>'
+              + '<p class="hover header-user">' + escapeIt(tweetData.user.handle) + '</p>'
               + '</header>';
 
   let footer = '<footer class="hover footer-tweet">'
-              + '<p class="hover footer-tweet-text">' + tweetData.content.text + '</p>'
+              + '<p class="hover footer-tweet-text">' + escapeIt(tweetData.content.text) + '</p>'
               + '<p class="hover footer-status">Created: ' + diffDays + ' days ago</p>'
               + '<i class="fa fa-heart"></i>'
               + '<i class="fa fa-refresh"></i>'
