22e43eee451b12c910491fba5eb51a20dceb04af
Wyatt Robinson <thewyatt@gmail.com>
2017-03-22 02:30:14 +0000

    Char-counter function complete.

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -4,8 +4,13 @@ $(document).ready(function () {
   console.log('document has loaded');
 
   $('.text-area').on('keyup', function() {
-    let remaining = $('.text-area').val();
+    let remaining = 140 - $('.text-area').val().length;
     console.log(remaining);
+    let charCounter = $(this).parent().children('span.counter')
+    charCounter.text(remaining);
+    if (remaining < 0) {
+      charCounter.css('color', 'red');
+    }
   });
 });
 
