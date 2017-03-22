e22d3bd3c37a1b2429f60f75f709f8033d792b7f
Paige <paigekato@gmail.com>
2017-03-22 02:57:31 +0000

    counter is working

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+  $('.new-tweet form').on('keydown', 'textarea', function(event) {
+  var count = $(this).parent().find('.counter');
+  var counter = 140 - $(this).val().length -1;
+  count.html(counter);
+    if(counter >= 0) {
+      $(count).css('color', 'black');
+       } else {
+         $(count).css('color', 'red');
+      }
+  })
+});
+
