7a3906bdf365b4c39d9ad3c3ed32b63570facd05
Paige <paigekato@gmail.com>
2017-03-23 20:02:50 +0000

    slide toggle

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,85 +4,96 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
- $(function () {
+$(function () {
 
-var tweetData = {
-  "user": {
-    "name": "Newton",
-    "avatars": {
-      "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
-      "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
-      "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
+  var tweetData = {
+    "user": {
+      "name": "Newton",
+      "avatars": {
+        "small": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
+        "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
+        "large": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
+      },
+      "handle": "@SirIsaac"
     },
-    "handle": "@SirIsaac"
-  },
-  "content": {
-    "text": "If I have seen further it is by standing on the shoulders of giants"
-  },
-  "created_at": 1461116232227
-}
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 1461116232227
+  };
 
 
- function createdTweetElement (tweetData) {
-  var name = tweetData.user.name;
-  var avatars = tweetData.user.avatars.small;
-  var handle = tweetData.user.handle;
-  var content = tweetData.content.text;
-  var date = tweetData.created_at;
+  function createdTweetElement (tweetData) {
+    var name = tweetData.user.name;
+    var avatars = tweetData.user.avatars.small;
+    var handle = tweetData.user.handle;
+    var content = tweetData.content.text;
+    var date = tweetData.created_at;
 
-  var $tweet = ($("<article>").addClass("tweeter"))
-    .append($("<header>")
-      .append($("<img>").attr("src", avatars).addClass("profile"))
-      .append($("<h3>").text(name))
-      .append($("<span>").addClass("handler").text(handle))
-    )
-    .append($("<p>").addClass("tweetBody").text(content))
-    .append($("<footer>").addClass("feets")
-      .append($("<span>").text(date))
+    var $tweet = ($("<article>").addClass("tweeter"))
+      .append($("<header>")
+        .append($("<img>").attr("src", avatars).addClass("profile"))
+        .append($("<h3>").text(name))
+        .append($("<span>").addClass("handler").text(handle))
+      )
+      .append($("<p>").addClass("tweetBody").text(content))
+      .append($("<footer>").addClass("feets")
+        .append($("<span>").text(date))
   );
-  return $tweet;
-}
+    return $tweet;
+  }
 
-function renderTweets(tweets) {
-  tweets.forEach(function(tweet) {
-    let $tweet = createdTweetElement(tweet);
-    $('#tweet-container').prepend($tweet);
+  function renderTweets(tweets) {
+    tweets.forEach(function(tweet) {
+      let $tweet = createdTweetElement(tweet);
+      $('#tweet-container').prepend($tweet);
+    });
+  }
+
+  $('#compose-button').click(function() {
+    $('.new-tweet').slideToggle(2000, function() {
+    });
   });
-}
 
+  $('#tweet-form').on('submit', function(event) {
+    event.preventDefault();
+    var input = $('#new-tweet-input').val();
+    var alertMessage = $('#alert-message');
+    if(input === '' || input === null) {
+      alertMessage.html('You post appears to be empty. Please enter a TWEET').fadeTo(5000, 0);
+      setTimeout(function() { alertMessage.html('').fadeTo(0, 1); }, 5000);
+    } else if (input.length > 140) {
+      alertMessage.html('Your post is too long. Please limit your tweet to 140 characters').fadeTo(5000, 0);
+      setTimeout(function() { alertMessage.html('').fadeTo(0, 1); }, 5000);
+    } else {
+      $.ajax({
+        url: '/tweets',
+        method: 'POST',
+        data: {
+          text: input
+        }
 
-
-$('#tweet-form').on('submit', function(event) {
-  event.preventDefault();
-  var newTweetsText = $('#new-tweet-input').val();
-  $.ajax({
-    url: '/tweets',
-    method: 'POST',
-    data: {
-      text: newTweetsText
+      }).done(function (tweet) {
+        let newTweet = createdTweetElement(tweet);
+        $('#tweet-container').prepend(newTweet);
+      });
+      $('#new-tweet-input').val('');
     }
-  }).done(function (tweet) {
-    let newTweet = createdTweetElement(tweet);
-    $('#tweet-container').prepend(newTweet);
   });
-  $('#new-tweet-input').val('');
-});
+  function loadTweets () {
+    $.ajax({
+      url: '/tweets',
+      method: 'GET'
+    }).success(function(tweets) {
+      renderTweets(tweets);
+    });
+  }
 
-
-function loadTweets () {
-  $.ajax({
-    url: '/tweets',
-    method: 'GET'
-  }).success(function(tweets) {
-     renderTweets(tweets);
-  });
-}
-
-loadTweets();
+  loadTweets();
 
 
 
 
 
 
-}) //end of $ready
+}); //end of $ready
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
   $('.new-tweet form').on('keydown', 'textarea', function(event) {
   var count = $(this).parent().find('.counter');
-  var counter = 140 - $(this).val().length -1;
+  var counter = 140 - $(this).val().length;
   count.html(counter);
     if(counter >= 0) {
       $(count).css('color', 'black');
