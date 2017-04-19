f6b42e98e3cee1f7b1892b09fe85d48586b64a3f
Elvis Chan <choectt@gmail.com>
2017-04-19 21:37:31 +0000

    Added ajax and form functionalities

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -18,11 +18,8 @@ function createTweetElement(tData) {
   const handle = tData.user.handle;
   const avatar = tData.user.avatars.small;
   const content = escape(tData.content.text);
-  const created = new Date(tData.created_at);
-  const createdYY = created.getFullYear();
-  const createdMM = created.getMonth();
-  const createdDD = created.getDate();
-
+  const dateAgo = moment(tData.created_at).fromNow();
+  // console.log(dateago);
   tweet = `<article>
             <header>
               <span class='avatar'><img src='${avatar}' width='55px' height='55px'></span>
@@ -31,7 +28,7 @@ function createTweetElement(tData) {
             </header>
             <div class='content'>${content}</div>
             <footer>
-              <span class='postdate'>${createdYY}/${createdMM}/${createdDD}</span>
+              <span class='postdate'>${dateAgo}</span>
               <div class='social'>
                 <span><i class='fa fa-flag' aria-hidden='true'></i></span>
                 <span><i class='fa fa-retweet' aria-hidden='true'></i></span>
@@ -44,61 +41,33 @@ function createTweetElement(tData) {
 }
 
 function renderTweets(tweets) {
+    $('.tweets-container').empty();
   tweets.forEach(function (element) {
     let tweet = createTweetElement(element);
-    $('.tweets-container').append(tweet);
+    $('.tweets-container').prepend(tweet);
   });
 }
 
-// Test / driver code (temporary). Eventually will get this from the server.
-// Fake data taken from tweets.json
-var data = [{
-    "user": {
-      "name": "Newton",
-      "avatars": {
-        "small": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
-        "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
-        "large": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
-      },
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": {
-        "small": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
-        "regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
-        "large": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
-      },
-      "handle": "@rd"
-    },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  },
-  {
-    "user": {
-      "name": "Johann von Goethe",
-      "avatars": {
-        "small": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
-        "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
-        "large": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
-      },
-      "handle": "@johann49"
-    },
-    "content": {
-      "text": "Es ist nichts schrecklicher als eine tätige Unwissenheit."
-    },
-    "created_at": 1461113796368
-  }
-];
-
 $(document).ready(function () {
-  renderTweets(data);
+  function loadTweets() {
+    $.ajax({
+      url: '/tweets',
+      method: 'GET',
+      dataType: "json",
+      success: renderTweets
+    });
+  }
+
+  loadTweets();
+
+  $("form").on("submit", function (event) {
+    event.preventDefault();
+    $.ajax({
+      url: '/tweets',
+      method: 'POST',
+      data: $(this).serialize(),
+      success: loadTweets
+    });
+  });
+
 });
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -2,12 +2,17 @@ function getCharRemaining() {
   $(this).siblings('.counter').text(140 - (Array.from(this.value).length));
 
   // color warning if over char limit
-  if ($(this).val().length > 140) {
-    $(this).siblings('.counter').css("color", "red");
+  if ($(this).val().length > 140)  {
+    $(this).siblings('.counter').css('color', 'red');
+    $(this).siblings('.submit-btn').attr('disabled', 'disabled');
+    $(this).siblings('.error').text('Exceeded character limit');
+
+  } else if ($(this).val().length < 1) {
     $(this).siblings('.submit-btn').attr('disabled', 'disabled');
   } else {
-    $(this).siblings('.counter').css("color", "black");
+    $(this).siblings('.counter').css('color', 'black');
     $(this).siblings('.submit-btn').removeAttr('disabled', 'disabled');
+    $(this).siblings('.error').text('');
   }
 }
 
