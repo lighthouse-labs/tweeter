e3c8982a662bf934793c3fe152ee6361ee23c4a5
Edurne Berastegi Zabalza <edurne.berastegi@gmail.com>
2017-04-20 20:59:35 +0000

    Data persistence using MongoDB

--- server/lib/data-helpers.js
+++ server/lib/data-helpers.js
@@ -1,26 +1,20 @@
 "use strict";
 
 // Simulates the kind of delay we see with network or filesystem operations
-const simulateDelay = require("./util/simulate-delay");
+//const simulateDelay = require("./util/simulate-delay");
 
 // Defines helper functions for saving and getting tweets, using the database `db`
 module.exports = function makeDataHelpers(db) {
   return {
 
-    // Saves a tweet to `db`
-    saveTweet: function(newTweet, callback) {
-      simulateDelay(() => {
-        db.tweets.push(newTweet);
-        callback(null, true);
-      });
-    },
+    getTweets: function (callback) {
+     db.collection("tweets").find().toArray(callback);
+   },
 
-    // Get all tweets in `db`, sorted by newest first
-    getTweets: function(callback) {
-      simulateDelay(() => {
-        const sortNewestFirst = (a, b) => a.created_at - b.created_at;
-        callback(null, db.tweets.sort(sortNewestFirst));
-      });
+   // Saves a tweet to `db`
+    saveTweet: function(newTweet, callback) {
+        db.collection("tweets").insertOne(newTweet);
+        callback(null, true);
     }
 
   };
