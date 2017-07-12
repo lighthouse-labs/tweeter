3ecc235128c81c1374a4274c61097f87acab5d3b
Marshall Danel <marshalldanel@gmail.com>
2017-07-11 22:48:18 -0700

    Initial tweet data/js commit

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,3 +4,92 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+// Test / driver code (temporary). Eventually will get this from the server.
+
+
+var data = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": {
+        "small": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
+        "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
+        "large": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
+      },
+      "handle": "@SirIsaac"
+    },
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 1461116232227
+  },
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": {
+        "small": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
+        "regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
+        "large": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
+      },
+      "handle": "@rd" },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+  },
+  {
+    "user": {
+      "name": "Johann von Goethe",
+      "avatars": {
+        "small": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
+        "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
+        "large": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
+      },
+      "handle": "@johann49"
+    },
+    "content": {
+      "text": "Es ist nichts schrecklicher als eine tätige Unwissenheit."
+    },
+    "created_at": 1461113796368
+  }
+];
+
+$(document).ready(function () {
+
+  function escape(str) {
+    var div = document.createElement('div');
+    div.appendChild(document.createTextNode(str));
+    return div.innerHTML;
+  }
+
+  function createTweetElement(tweet) {
+    const newTime = new Date(tweet.created_at * 1000);
+    const html = `
+      <article class="tweets">
+        <header>
+          <img src=${tweet.user.avatars.small} alt="profile pic">
+          <h3>${tweet.user.name}</h3>
+          <p>${tweet.user.handle}</p>
+        </header>
+        <main>
+          <p>${escape(tweet.content.text)}</p>
+        </main>
+        <footer>
+          <p>${newTime.toUTCString()}</p>
+          <span class="fa fa-flag" aria-hidden="true"></span>
+          <span class="fa fa-retweet" aria-hidden="true"></span>
+          <span class="fa fa-heart" aria-hidden="true"></span>
+        </footer>
+      </article>`;
+    return html;
+  }
+  function renderTweets(data) {
+    data.forEach((tweet) => {
+      $('#tweets').prepend(createTweetElement(tweet));
+    });
+  }
+  renderTweets(data);
+});
+
+
+
