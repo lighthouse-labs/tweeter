cd35f1aee8712ff38c4320c0ed03e3a817ef9f8b
Juan Victor Cortez <juanvictor.cortez@gmail.com>
2017-07-13 18:09:04 +0000

    added fetching tweet function to the app

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,60 +1,7 @@
-/*
- * Client-side JS logic goes here
- * jQuery is already loaded
- * Reminder: Use (and do all your DOM work in) jQuery's document ready function
- */
 $(function () {
 
-    var data = [
-        {
-            "user": {
-                "name": "Newton",
-                "avatars": {
-                    "small": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
-                    "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
-                    "large": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
-                },
-                "handle": "@SirIsaac"
-            },
-            "content": {
-                "text": "If I have seen further it is by standing on the shoulders of giants"
-            },
-            "created_at": 1461116232227
-        },
-        {
-            "user": {
-                "name": "Descartes",
-                "avatars": {
-                    "small": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
-                    "regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
-                    "large": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
-                },
-                "handle": "@rd"
-            },
-            "content": {
-                "text": "<script>alert('uh oh!');</script>"
-            },
-            "created_at": 1461113959088
-        },
-        {
-            "user": {
-                "name": "Johann von Goethe",
-                "avatars": {
-                    "small": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
-                    "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
-                    "large": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
-                },
-                "handle": "@johann49"
-            },
-            "content": {
-                "text": "Es ist nichts schrecklicher als eine tätige Unwissenheit."
-            },
-            "created_at": 1461113796368
-        }
-    ];
-
     function createTweetElement(tweetData) {
-        const timeRead = new Date(tweetData.created_at * 1000);
+        const timeRead = new Date(tweetData.created_at);
         const html = `
     <article class="tweet">
         <header class="tweet-header">
@@ -66,29 +13,84 @@ $(function () {
         <p class="tweet-body">${escape(tweetData.content.text)}</p>
         </main>
         <footer class="tweet-footer">
-        <p class="tweet-timestamp">${timeRead.toUTCString()}</p>
+        <p class="tweet-timestamp">${timeRead}</p>
+        <div class="icons">
+        <i class="fa fa-flag" aria-hidden="true"></i>
+        <i class="fa fa-retweet" aria-hidden="true"></i>
+        <i class="fa fa-heart" aria-hidden="true"></i>
+        </div>
         </footer>
     </article>`;
         return html;
     }
-    
+
     function escape(str) {
         var div = document.createElement('div');
         div.appendChild(document.createTextNode(str));
         return div.innerHTML;
     }
 
-
-    // var $tweet = createTweetElement(tweetData);
-    function renderData(data) {
-        data.forEach((tweet) => {
+//all tweets are retweeted along with new tweet
+    //  let allTweets = "";
+    //     for (const oneTweet in tweets) {
+    //         const renderedTweet = createTweetElement(tweets[tweet])
+    //         allTweets = renderedTweet + allTweets
+    //     }
+    //         $('#tweets-container').append(createTweetElement(allTweets))
+        
+    // }
+    function renderData(tweets) {
+        tweets.forEach((tweet) => {
             $('#tweets-container').prepend(createTweetElement(tweet))
         })
     };
 
-    renderData(data);
-    // }
-    // Test / driver code (temporary)
-    // console.log($tweet); // to see what it looks like
+    const $textBox = $('.form-layout');
 
+    function charIsInvalid(characterCheck) {
+        const tweetCharNumber = $(characterCheck).children().find(".text-box").context["0"].value.length
+        if (tweetCharNumber > 140 || tweetCharNumber === 0) {
+            return true
+        } else {
+            return false
+        }
+    }
+    function generateTweet(event) {
+        const $form = $(this);
+        event.preventDefault()
+        const charValidation = charIsInvalid($form)
+        if (charValidation === true) {
+            alert("Error! Too long or too short")
+        } else {
+            $.ajax({
+                type: 'POST',
+                url: "/tweets",
+                data: $form.serialize()
+            })
+            .done(() => {
+                loadtweets(event)
+                $form.find("textarea").val("")
+                $form.find(".counter").text("140")
+            })
+        }
+    }
+
+    function loadtweets() {
+        $.getJSON('/tweets')
+            .done((tweets) => {
+            })
+            .done((tweets) => {
+                renderData(tweets)
+            })
+    }
+    
+    loadtweets()
+    $textBox.on('submit', generateTweet)
+    $(".compose-action").click(function () {
+        $(".new-tweet").slideToggle(function () {
+            if ($(".new-tweet").is(":visible")) {
+                $(".new-tweet > form > textarea").focus();
+            }
+        })
+    });
 });
