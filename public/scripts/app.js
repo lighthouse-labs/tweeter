3afd09f9dae455ff4637d969b8b1e8eb8be1c9ca
John <john.wong.h@gmail.com>
2017-07-13 19:31:48 +0000

    Added compose button that hides/shows tweet form. Added Ajax functions to main page to stop refresh on page when new tweet is created.

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,61 +1,5 @@
 $(document).ready(function(){
 
-  var data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": {
-        "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
-        "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
-        "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
-      },
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": {
-        "small":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
-        "regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
-        "large":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
-      },
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  },
-  {
-    "user": {
-      "name": "Johann von Goethe",
-      "avatars": {
-        "small":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
-        "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
-        "large":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
-      },
-      "handle": "@johann49"
-    },
-    "content": {
-      "text": "Es ist nichts schrecklicher als eine tätige Unwissenheit."
-    },
-    "created_at": 1461113796368
-  }
-];
-
-function renderTweets(tweets) {
-tweets.forEach(function(item){
-
-$('#tweets-container').prepend(createTweetElement(item)); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
-
-})
-}
-
-
 function createTweetElement (data) {
   const createdTime = new Date(data.created_at*1000)
 
@@ -90,19 +34,72 @@ return $tweet;
 // Test / driver code (temporary)
 // console.log($tweet); // to see what it looks like
 }
-renderTweets(data);
 
-document.getElementById("tweetSubmit").addEventListener("click", function(event) {
-  event.preventDefault()
-});
 
-function loadTweets {
-  $.ajax({
-    type: 'GET'
-    url: '/tweets'
+function renderTweets(tweets) {
+  $('tweets-container').children().remove();
+  tweets.forEach(function(item){
+  $('#tweets-container').prepend(createTweetElement(item)); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
+})
+}
 
+// document.getElementById("tweetSubmit").addEventListener("click", function(event) {
+//   event.preventDefault()
+// });
+
+
+
+function loadTweets () {
+  $.getJSON('/tweets')
+  .done((tweets) => {
+    console.log(tweets)
+  })
+  .done((tweets) => {
+    renderTweets(tweets)
   })
 }
+
+function formValidation () {
+  var text = $('.new-tweet > #tweetSubmit > textarea')
+  if (!text.val().length) {
+    alert('Enter message!')
+    return false
+  }
+  if (text.val().length > 140) {
+    alert('Please shorten your message')
+    return false
+  }
+  return true
+}
+
+
+function createTweets () {
+  event.preventDefault();
+  if (formValidation()) {
+    const $form = $(this);
+    $.ajax({
+      type: 'POST',
+      url: '/tweets',
+      data: $form.serialize()
+    })
+    .done(() => {
+      loadTweets()
+    })
+  }
+}
+
+const $form = $('#tweetSubmit');
+$form.on('submit', createTweets)
+loadTweets()
+
+
+$( ".button" ).click(function() {
+  $( ".new-tweet" ).slideToggle( "slow", function() {
+  });
+  $("#tweetTextArea").focus()
+});
+
+
 
 });
 /*
