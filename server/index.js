3fc2dd3ffb896886240e7441eef99a235efde0bb
Juan Victor Cortez <juanvictor.cortez@gmail.com>
2017-07-13 22:01:47 +0000

    connected to MongoDB to make tweets persist

--- server/index.js
+++ server/index.js
@@ -2,33 +2,49 @@
 
 // Basic express setup:
 
-const PORT          = 8080;
-const express       = require("express");
-const bodyParser    = require("body-parser");
-const app           = express();
+const PORT = 8080;
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 // The in-memory database of tweets. It's a basic object with an array in it.
-const db = require("./lib/in-memory-db");
+// const db = require("./lib/in-memory-db");
+const MongoCLient = require("mongodb").MongoClient;
+const MONGODB_URI = "mongodb://localhost:27017/tweeter"
+// Defines helper functions for saving and getting tweets, using the database `db`
+MongoCLient.connect(MONGODB_URI, (err, db) => {
+  if (err) {
+    console.log(`Failed to connect: ${MONGODB_URI}`)
+    throw err;
+  }
+  console.log(`Connected to mongodb: ${MONGODB_URI}`)
+  // db.close();
+  const DataHelpers = require("./lib/data-helpers.js")(db);
+  DataHelpers.getTweets(function (err, tweets) {
+  });
 
-// The `data-helpers` module provides an interface to the database of tweets.
-// This simple interface layer has a big benefit: we could switch out the
-// actual database it uses and see little to no changes elsewhere in the code
-// (hint hint).
-//
-// Because it exports a function that expects the `db` as a parameter, we can
-// require it and pass the `db` parameter immediately:
-const DataHelpers = require("./lib/data-helpers.js")(db);
+  const tweetsRoutes = require("./routes/tweets")(DataHelpers);
 
-// The `tweets-routes` module works similarly: we pass it the `DataHelpers` object
-// so it can define routes that use it to interact with the data layer.
-const tweetsRoutes = require("./routes/tweets")(DataHelpers);
+  // Mount the tweets routes at the "/tweets" path prefix:
+  app.use("/tweets", tweetsRoutes);
 
-// Mount the tweets routes at the "/tweets" path prefix:
-app.use("/tweets", tweetsRoutes);
+  app.listen(PORT, '0.0.0.0' ,() => {
+    console.log("Example app listening on port " + PORT);
+  })
+  // The `data-helpers` module provides an interface to the database of tweets.
+  // This simple interface layer has a big benefit: we could switch out the
+  // actual database it uses and see little to no changes elsewhere in the code
+  // (hint hint).
+  //
+  // Because it exports a function that expects the `db` as a parameter, we can
+  // require it and pass the `db` parameter immediately:
 
-app.listen(PORT, () => {
-  console.log("Example app listening on port " + PORT);
+
+
+  // The `tweets-routes` module works similarly: we pass it the `DataHelpers` object
+  // so it can define routes that use it to interact with the data layer.
+
 });
--- server/lib/data-helpers.js
+++ server/lib/data-helpers.js
@@ -1,27 +1,26 @@
 "use strict";
 
 // Simulates the kind of delay we see with network or filesystem operations
-const simulateDelay = require("./util/simulate-delay");
 
 // Defines helper functions for saving and getting tweets, using the database `db`
+
 module.exports = function makeDataHelpers(db) {
   return {
 
     // Saves a tweet to `db`
-    saveTweet: function(newTweet, callback) {
-      simulateDelay(() => {
-        db.tweets.push(newTweet);
-        callback(null, true);
-      });
+    saveTweet: function (newTweet, callback) {
+      // simulateDelay(() => {
+      //   db.tweets.insert(newTweet);
+      //   callback(null, true);
+      // });
+      db.collection('tweets').insertOne(newTweet, callback)
     },
 
     // Get all tweets in `db`, sorted by newest first
-    getTweets: function(callback) {
-      simulateDelay(() => {
-        const sortNewestFirst = (a, b) => a.created_at - b.created_at;
-        callback(null, db.tweets.sort(sortNewestFirst));
-      });
+    getTweets: function (newTweet) {
+      // simulateDelay(() => {
+      // const sortNewestFirst = (a, b) => a.created_at - b.created_at;
+      db.collection("tweets").find().toArray(newTweet);
     }
-
-  };
+  }
 }
