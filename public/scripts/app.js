39e7ee0b183c5e60b57c7a711bfcc10e4e1745ac
MattEmond <memond36@gmail.com>
2017-11-14 23:56:29 +0000

    createTweetElement and renderTweets functions both added and working; index.html cleaned up and holder tweet removed

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -3,5 +3,90 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
+function createTweetElement(tweet) {
+  let $tweet = `
+  <article class="tweet">
+      <header class="tweet-header">
+      <img class="tweet-logo" src="${tweet.user.avatars.small}">
+      <div class="tweet-username">${tweet.user.name}</div>
+      <div class="tweet-tag">${tweet.user.handle}</div>
+      </header>
+      <p class="tweet-text">${tweet.content.text}</p>
+      <footer class="tweet-footer">
+        <div class="tweet-icons">
+          <i class="fa fa-flag" aria-hidden="true"></i>
+          <i class="fa fa-retweet" aria-hidden="true"></i>
+          <i class="fa fa-heart" aria-hidden="true"></i>
+         </div>
+        <p>${tweet.created_at}</p>
+      </footer>
+      </article>
+  `;
+    return $tweet;
+}
 
 
+function renderTweets(tweets) {
+  // loops through tweets
+  // calls createTweetElement for each tweet
+  // takes return value and appends it to the tweets container
+  for (tweet of tweets) {
+    console.log(`tweet is : ${tweet}`)
+    $('#tweets-container').append(createTweetElement(tweet));
+  }
+}
+
+
+
+var data = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": {
+        "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
+        "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
+        "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
+      },
+      "handle": "@SirIsaac"
+    },
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 1461116232227
+  },
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": {
+        "small":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
+        "regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
+        "large":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
+      },
+      "handle": "@rd" },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+  },
+  {
+    "user": {
+      "name": "Johann von Goethe",
+      "avatars": {
+        "small":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
+        "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
+        "large":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
+      },
+      "handle": "@johann49"
+    },
+    "content": {
+      "text": "Es ist nichts schrecklicher als eine tätige Unwissenheit."
+    },
+    "created_at": 1461113796368
+  }
+];
+
+
+
+$(document).ready(function() {
+  renderTweets(data);
+})
