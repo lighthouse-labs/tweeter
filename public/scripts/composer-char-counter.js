01ccfc1f6dea06883d95058280a7be7839731cab
Joh Yoshida <johyoshida@gmail.com>
2017-11-14 13:05:42 -0800

    Show remaining characters and use .too-many-characters class when tweet length exceeds charLength

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,6 +1,15 @@
 $(document).ready(function() {
-  $("form").on("keypress", "textarea", function(event) {
-    console.log("Hit!");
+  let charLength = 140;
+  $(".new-tweet form").on("keyup", "textarea", function(event) {
+    // console.log(140 - $(this).val().length);
+    $(this).parent().children(".counter").text(charLength - $(this).val().length);
+    if ($(this).val().length >= charLength) {
+      console.log("too long!");
+      $(this).parent().children(".counter").addClass("too-many-chars");
+    } else {
+      $(this).parent().children(".counter").removeClass("too-many-chars");
+    }
   });
+
 
 });
