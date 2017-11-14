f1105427fb089d10a30cef94e793760a12f7d887
Joh Yoshida <johyoshida@gmail.com>
2017-11-14 13:27:38 -0800

    Minor refactor

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,13 +1,13 @@
 $(document).ready(function() {
   let charLength = 140;
   $(".new-tweet form").on("keyup", "textarea", function(event) {
-    // console.log(140 - $(this).val().length);
-    $(this).parent().children(".counter").text(charLength - $(this).val().length);
+    const counter = $(".new-tweet .counter");
+    counter.text(charLength - $(this).val().length);
     if ($(this).val().length >= charLength) {
       console.log("too long!");
-      $(this).parent().children(".counter").addClass("too-many-chars");
+      counter.addClass("error");
     } else {
-      $(this).parent().children(".counter").removeClass("too-many-chars");
+      counter.removeClass("error");
     }
   });
 
