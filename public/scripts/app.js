fca6437bd2c50166a936f586c85b9445c6b8ef1c
MattEmond <memond36@gmail.com>
2017-11-14 22:24:16 +0000

    Added hover functions to tweets; buttons added in footer

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,3 +4,11 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+$(document).ready(function() {
+// $(".tweet").hover(function() {
+//   $(this).find('.tweet__icons').addClass('tweet__icons--hover')
+// }, function() {
+//   $(this).find('.tweet__icons').removeClass('tweet__icons--hover')
+// });
+
+});
