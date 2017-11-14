a6271addbac992ea5cb072347db38c45808f013d
MattEmond <memond36@gmail.com>
2017-11-14 18:54:15 +0000

    Counter now fully functional; turns red when above char limit

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -4,8 +4,12 @@ $('textarea').keyup(function() {
   let maxLength = 140;
   let length1 = $(this).val().length;
   let length2 = maxLength - length1;
-  $('.counter').text(length2);
+  $(this).siblings('.counter').text(length2);
+  if (length2 < 0) {
+    $(this).siblings('.counter').addClass('red');
+  } else {
+    $(this).siblings('.counter').removeClass('red');
+  }
 });
-
 });
 
