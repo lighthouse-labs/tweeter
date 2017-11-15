77f9e471cdd45f534d83f420989d7e7d632fc3a3
MattEmond <memond36@gmail.com>
2017-11-15 19:16:11 +0000

    Tweets are now able to be submitted from the text area, and will show up on the main page via ajax

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -3,6 +3,8 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
+
+
 function createTweetElement(tweet) {
   let $tweet = `
   <article class="tweet">
@@ -25,75 +27,41 @@ function createTweetElement(tweet) {
   return $tweet;
 }
 
+// takes input from textarea and appends all data from createTweetElement function
 function renderTweets(tweets) {
   for (tweet of tweets) {
     $('#tweets-container').append(createTweetElement(tweet));
   }
 }
 
-
-
-
-
-var data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": {
-        "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
-        "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
-        "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
-      },
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": {
-        "small":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
-        "regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
-        "large":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
-      },
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  },
-  {
-    "user": {
-      "name": "Johann von Goethe",
-      "avatars": {
-        "small":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
-        "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
-        "large":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
-      },
-      "handle": "@johann49"
-    },
-    "content": {
-      "text": "Es ist nichts schrecklicher als eine tätige Unwissenheit."
-    },
-    "created_at": 1461113796368
-  }
-];
-
-$(document).ready(function() {
-  renderTweets(data);
-
-$("form").on("submit", function(event) {
-  event.preventDefault();
-  console.log($(this).serialize());
+// when a tweet is submitted, fetch data from /tweets.
+function loadTweets(arr) {
   $.ajax({
-    url: "/tweets",
-    method: "POST",
-    data: $(this).serialize(),
-    success: renderTweets
+    url: '/tweets',
+    method: 'GET',
+    format: 'json',
+    success: function (tweets) {
+      renderTweets(tweets)
+    }
   });
-  });
+}
 
+// on document load
+$(document).ready(function() {
+  loadTweets()
+  // when a new tweet is submitted post the result to /tweets, but do not leave page
+  $("form").on("submit", function(event) {
+    event.preventDefault();
+    console.log($(this).serialize());
+    $.ajax({
+      url: "/tweets",
+      method: "POST",
+      data: $(this).serialize(),
+      // on success, pass in the loadTweets function and remove text from the textarea
+      success: function () {
+        loadTweets()
+        $("#tweet-box").val('');
+      }
+    });
+  });
 });
