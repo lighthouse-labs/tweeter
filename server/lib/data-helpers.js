8488f9fd160c3b2beb8af2664e632113815df63f
Joh Yoshida <johyoshida@gmail.com>
2017-11-16 13:20:04 -0800

    Switched database to MongoDB

--- server/lib/data-helpers.js
+++ server/lib/data-helpers.js
@@ -17,10 +17,11 @@ module.exports = function makeDataHelpers(db) {
 
     // Get all tweets in `db`, sorted by newest first
     getTweets: function(callback) {
-      simulateDelay(() => {
-        const sortNewestFirst = (a, b) => a.created_at - b.created_at;
-        callback(null, db.tweets.sort(sortNewestFirst));
-      });
+      db.collection("tweets").find().toArray(callback);
+      // simulateDelay(() => {
+      //   const sortNewestFirst = (a, b) => a.created_at - b.created_at;
+      //   callback(null, db.tweets.sort(sortNewestFirst));
+      // });
     }
 
   };
