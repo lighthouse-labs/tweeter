28969520f5ce489318a2536a4c35e5f0d631e9ab
Joh Yoshida <johyoshida@gmail.com>
2017-11-17 16:32:06 -0800

    Implemented likes counter

---
+++ public/scripts/likes-counter.js
@@ -0,0 +1,27 @@
+$(document).ready(() => {
+  setTimeout(() => {
+    $("span.heart").click((event) => {
+      let heart = $(event.currentTarget);
+      let likes = parseInt(heart.children(".heart").text());
+      if (heart.children(".heart").hasClass("fa-heart-o")) {
+        heart.children(".heart").removeClass("fa-heart-o").addClass("fa-heart");
+        heart.children(".heart").text(likes + 1);
+
+      } else if (heart.children(".heart").hasClass("fa-heart")) {
+        heart.children(".heart").removeClass("fa-heart").addClass("fa-heart-o");
+        heart.children(".heart").text(likes - 1);
+      }
+
+      // $.ajax({
+      //   url: 'tweets/likes',
+      //   method: 'POST',
+      //   data: likes
+      // })
+      //   .then(function() {
+      //     $("#tweet-field").val("");
+      //     loadTweets();
+      //   });
+
+    });
+  }, 500);
+})
