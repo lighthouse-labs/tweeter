9e311182c2eadfc26819b734caa515ccaccad869
Joh Yoshida <johyoshida@gmail.com>
2017-11-17 16:31:10 -0800

    Added likes to tweet object

--- server/routes/tweets.js
+++ server/routes/tweets.js
@@ -29,7 +29,8 @@ module.exports = function(DataHelpers) {
       content: {
         text: req.body.text
       },
-      created_at: Date.now()
+      created_at: Date.now(),
+      likes: 0
     };
 
     DataHelpers.saveTweet(tweet, (err) => {
