12444e66f76290f525f402bd5dd67c27f2358dbb
Zushisan <74838@supinfo.com>
2018-01-30 01:37:34 +0000

    Tweet are now displayed using jquery to render them from a hard coded object

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -6,12 +6,83 @@
 
 // A $( document ).ready() block.
 $( document ).ready(function() {
-  // console.log("I AM LOADED");
 
-  // // This is the tweet character counter, turns red when <= 0
-  // $('#display-tweets .full-name').on('mouseover', function(){
-  //   console.log("hellllo");
-  //   $(this).css({"color":"#219fa5"});
-  // });
+  // Fake data taken from tweets.json
+  const data = [
+    {
+      "user": {
+        "name": "Newton",
+        "avatars": {
+          "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
+          "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
+          "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
+        },
+        "handle": "@SirIsaac"
+      },
+      "content": {
+        "text": "If I have seen further it is by standing on the shoulders of giants"
+      },
+      "created_at": 1461116232227
+    },
+    {
+      "user": {
+        "name": "Descartes",
+        "avatars": {
+          "small":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
+          "regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
+          "large":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
+        },
+        "handle": "@rd" },
+      "content": {
+        "text": "Je pense , donc je suis"
+      },
+      "created_at": 1461113959088
+    },
+    {
+      "user": {
+        "name": "Johann von Goethe",
+        "avatars": {
+          "small":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
+          "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
+          "large":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
+        },
+        "handle": "@johann49"
+      },
+      "content": {
+        "text": "Es ist nichts schrecklicher als eine tätige Unwissenheit."
+      },
+      "created_at": 1461113796368
+    }
+  ];
+
+  function createTweetElement(tweet) {
+
+    let fullName = tweet.user.name;
+    let avatarUrl = tweet.user.avatars.small;
+    let atName = tweet.user.handle;
+    let tweetContent = tweet.content.text;
+
+    let utcSeconds = tweet.created_at;
+    let timeStamp = Date(tweet.created_at).toString();
+
+
+    let $tweet = $('<div id="tweet-container"><div class="header"><header><img alt="vanil12" src="' + avatarUrl + '"><span class="full-name"> ' + fullName + ' </span><span class="at-name"> ' + atName + ' </span></header></div><article><p> ' + tweetContent + ' </p></article><footer><span> ' + timeStamp + ' </span><div class="icons"><i class="fa fa-flag mini-icons" aria-hidden="true"></i><i class="fa fa-retweet mini-icons" aria-hidden="true"></i><i class="fa fa-heart mini-icons" aria-hidden="true"></i></div></footer></div>');
+
+    return $tweet;
+
+  }
+// preppend()
+
+  function renderTweets(tweets) {
+    // loops through tweets
+    for(tweet in tweets){
+      // calls createTweetElement for each tweet
+      let fullTweet = createTweetElement(tweets[tweet]);
+      // takes return value and appends it to the tweets container
+      $('#display-tweets').append(fullTweet);
+    }
+  }
+
+  renderTweets(data);
 
 }); // Document ready end
