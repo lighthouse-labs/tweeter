8214648fca255c60a984c2c81fec508e02e939e3
Hemantkumar Panchal <panchalhemant2001@yahoo.com>
2018-04-25 01:46:49 +0000

    dynamic tweets functionality added

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -8,6 +8,63 @@
 $('document').ready(function() {
 
 
+// Fake data taken from tweets.json
+const data = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": {
+        "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
+        "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
+        "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
+      },
+      "handle": "@SirIsaac"
+    },
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 1461116232227
+  },
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": {
+        "small":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
+        "regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
+        "large":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
+      },
+      "handle": "@rd" },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+  },
+  {
+    "user": {
+      "name": "Johann von Goethe",
+      "avatars": {
+        "small":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
+        "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
+        "large":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
+      },
+      "handle": "@johann49"
+    },
+    "content": {
+      "text": "Es ist nichts schrecklicher als eine tätige Unwissenheit."
+    },
+    "created_at": 1461113796368
+  }
+];
+
+
+
+
+
+
+
+
+
+  //function that creates a dynamic tweet for the single tweet object
   function createTweetElement(tweetObj) {
     let $tweet = $('<article>').addClass('tweet');
 
@@ -25,7 +82,7 @@ $('document').ready(function() {
     //let $iLogo = $('<i>').addClass('far fa-smile');
 
     let $iLogo = $('<img>');
-    $iLogo.attr("src", tweetData.user.avatars.small);
+    $iLogo.attr("src", tweetObj.user.avatars.small);
     $iLogo.attr("align", "top");
     let $label = $('<label>');
     $label.text(tweetObj["user"]["name"]);
@@ -70,6 +127,29 @@ $('document').ready(function() {
 
 
 
+
+
+  //function that renders the dynamic tweets on html page for array of tweets object passed to it
+  function renderTweets(tweets) {
+    //loops through tweets
+      //calls createTweetElement for each tweet
+      //takes return value and appends it to the tweets container
+
+      for(let tweetObj of tweets) {
+        let $tweet = createTweetElement(tweetObj);
+        $('#realtweets').append($tweet);
+      }
+  }
+
+
+
+
+  renderTweets(data);
+
+/*
+
+
+
   // Test / driver code (temporary). Eventually will get this from the server.
   let tweetData = {
     "user": {
@@ -88,13 +168,13 @@ $('document').ready(function() {
   }
 
 
-  //tweetData = JSON.parse(tweetData);
+ //tweetData = JSON.parse(tweetData);
   var $tweet = createTweetElement(tweetData);
 
   // Test / driver code (temporary)
   console.log($tweet); // to see what it looks like
   $('#realtweets').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
 
-
+*/
 
 });   //ready() function ends here
