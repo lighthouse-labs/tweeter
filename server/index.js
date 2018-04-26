297a0551d89720645fd0b831e18344bf5338327f
camueljakcson <your@email.com>
2018-04-26 16:12:06 +0000

    container, logo, nav all in right position, white container scrolling properly and dissapearing behind header **except for placeholder text* tweet box properly formatted

--- server/index.js
+++ server/index.js
@@ -29,6 +29,10 @@ const tweetsRoutes = require("./routes/tweets")(DataHelpers);
 // Mount the tweets routes at the "/tweets" path prefix:
 app.use("/tweets", tweetsRoutes);
 
+app.post("/tweets", (req, res) => {
+  res.render('index');
+})
+
 app.listen(PORT, () => {
   console.log("Example app listening on port " + PORT);
 });
