a168f140cabd9fe85d77d4dae268cb2c5661b954
Jeff <jeff.tang@live.com>
2018-05-21 15:48:19 -0400

    initial commit, changed font and added navbar css

--- server/index.js
+++ server/index.js
@@ -2,12 +2,14 @@
 
 // Basic express setup:
 
-const PORT          = 8080;
-const express       = require("express");
-const bodyParser    = require("body-parser");
-const app           = express();
+const PORT = 8080;
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
 
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 app.use(express.static("public"));
 
 // The in-memory database of tweets. It's a basic object with an array in it.
