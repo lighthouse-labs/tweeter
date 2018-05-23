eabf4faa91df02d5fe3d1425d7beb858735e4c4a
Jeff <jeff.tang@live.com>
2018-05-23 18:26:36 -0400

    added handlers for tweet errors, created compose button

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -6,6 +6,12 @@
 
 $(document).ready(function () {
 
+  function escape(str) {
+    var div = document.createElement('div');
+    div.appendChild(document.createTextNode(str));
+    return div.innerHTML;
+  }
+
   function createTweetElement(tweetInput) {
     let $tweet = (`
     <article class='tweet'>
@@ -13,10 +19,10 @@ $(document).ready(function () {
         <span class='avatar'>
           <img src='${tweetInput.user.avatars.small}' class='avatar'>
         </span>
-        <span class='name'>${tweetInput.user.name}</span>
-        <span class='handle'>${tweetInput.user.handle}</span>
+        <span class='name'>${(escape(tweetInput.user.name))}</span>
+        <span class='handle'>${(escape(tweetInput.user.handle))}</span>
       </header>
-      <section class='tweet'>${tweetInput.content.text}</section>
+      <section class='tweet'>${(escape(tweetInput.content.text))}</section>
       <footer>
         <span class='date'>${tweetInput.created_at}</span>
         <span class='icons'>
@@ -44,11 +50,26 @@ $(document).ready(function () {
     })
   }
 
+  $("#compose").click(function () {
+    $(".new-tweet").slideToggle(function () {
+      $("textarea").focus()
+    })
+  })
+
   $("form").submit(function (event) {
     event.preventDefault();
-    $.post("/tweets", ($(this).serialize()))
-      .then(loadTweets)
-    $(this).trigger("reset")
+    // console.log(this)
+    let tweetLen = $(this).children("textarea").val().length
+    if (tweetLen === 0) {
+      alert("Tweets can't be empty")
+    } else if (tweetLen > 140) {
+      alert("Tweets can't be greater than 140 characters")
+    } else {
+      $.post("/tweets", ($(this).serialize()))
+        .then(loadTweets)
+      $(this).trigger("reset")
+      $(this).children(".counter").text(140)
+    }
   });
 
   loadTweets();
