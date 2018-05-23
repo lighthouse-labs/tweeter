6c34ce4db06d76865045e2cac28fb0e97d10ebbf
Matt Greff <greff.matt@icloud.com>
2018-05-23 18:30:02 +0000

    function for rendering and iterating through tweet data

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,6 +1,69 @@
-/*
- * Client-side JS logic goes here
- * jQuery is already loaded
- * Reminder: Use (and do all your DOM work in) jQuery's document ready function
- */
+// Test / driver code (temporary). Eventually will get this from the server.
+const data = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": {
+        "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
+        "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
+        "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
+      },
+      "handle": "@SirIsaac"
+    },
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 1461116232227
+  },
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": {
+        "small":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
+        "regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
+        "large":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
+      },
+      "handle": "@rd" },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+  },
+  {
+    "user": {
+      "name": "Johann von Goethe",
+      "avatars": {
+        "small":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
+        "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
+        "large":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
+      },
+      "handle": "@johann49"
+    },
+    "content": {
+      "text": "Es ist nichts schrecklicher als eine tätige Unwissenheit."
+    },
+    "created_at": 1461113796368
+  }
+];
+$(document)
+  .ready(function () {
+    function createTweetElement(database) {
+      const avatar = `<img src=${database.user.avatars.small}></img>`,
+            user_name = `<h2>${database.user.name}</h2>`,
+            header = `<header>${avatar}${user_name}<p>${database.user.handle}</p></header>`,
+            content = `<div>${database.content.text}</div>`,
+            footer = `<footer>${database.created_at}</footer>`
 
+      return $(`<article class='tweet'>${header}${content}${footer}</article>`)
+    }
+
+  function renderTweets (data){
+    data.forEach(function (tweet){
+      $tweet = createTweetElement(tweet);
+      $('#tweets-container').append($tweet);
+    });
+  }
+  renderTweets(data);
+    // Test / driver code (temporary)
+    //$('#tweets-container').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
+  });
