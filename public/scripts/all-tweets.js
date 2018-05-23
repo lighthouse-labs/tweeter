8057fe0571614596b0f868499b07122ad10c770a
Matt Greff <greff.matt@icloud.com>
2018-05-23 18:24:38 +0000

    adding hovering full opacity for tweets

---
+++ public/scripts/all-tweets.js
@@ -0,0 +1,8 @@
+$(document).ready( function(){
+    $(".tweet").fadeTo('slow', 0.75)
+    $(".tweet").hover(function (){
+      $(this).fadeTo('slow', 1)
+      },function () {    
+      $(this).fadeTo('slow', 0.75)
+    });
+});
