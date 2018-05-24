193e0ef67290e686314b894d12ba5daffe15afc6
Jeff <jeff.tang@live.com>
2018-05-24 14:30:40 -0400

    modified to use mongodb for get and post

--- server/lib/data-helpers.js
+++ server/lib/data-helpers.js
@@ -7,19 +7,32 @@ const simulateDelay = require("./util/simulate-delay");
 module.exports = function makeDataHelpers(db) {
   return {
 
+    // // Saves a tweet to `db`
+    // saveTweet: function (newTweet, callback) {
+    //   simulateDelay(() => {
+    //     db.tweets.push(newTweet);
+    //     callback(null, true);
+    //   });
+    // },
     // Saves a tweet to `db`
     saveTweet: function (newTweet, callback) {
       simulateDelay(() => {
-        db.tweets.push(newTweet);
+        db.collection("tweeter").insertOne(newTweet)
         callback(null, true);
       });
     },
 
     // Get all tweets in `db`, sorted by newest first
+    // getTweets: function(callback) {
+    //   simulateDelay(() => {
+    //     const sortNewestFirst = (a, b) => a.created_at - b.created_at;
+    //     callback(null, db.tweets.sort(sortNewestFirst));
+    //   });
+    // }
     getTweets: function (callback) {
-        db.collection("tweeter").find({}).toArray(callback)
-      
-
+      simulateDelay(() => {
+        db.collection("tweeter").find({}).sort({ created_at: 1 }).toArray(callback)
+      })
     }
   }
 }
