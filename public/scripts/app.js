763000d6e61be0c8daebf6b618fe35082676be56
KimonoKurtRussell <n_hardin@hotmail.com>
2018-05-24 17:19:59 +0000

    added compose toggle functionality, additional css, and auto selecting text field on compose push

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,31 +1,7 @@
-/*
- * Client-side JS logic goes here
- * jQuery is already loaded
- * Reminder: Use (and do all your DOM work in) jQuery's document ready function
- */
-
-const tweetData = {
-  "user": {
-      "name": "Newton",
-      "avatars": {
-        "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
-        "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
-        "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
-      },
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
- }
-
-
-
+$(document).ready(function() {
 
 
 function createTweetElement(tweet) {
-
   var $tweet = $("<div>").addClass("tweet");
   var $header = $("<header>").appendTo($tweet);
   $("<img>").addClass("profile-pic").attr("src", tweet.user.avatars.small).appendTo($header);
@@ -38,13 +14,63 @@ function createTweetElement(tweet) {
   $("<i>").addClass("fa fa-thumbs-o-up").appendTo($span);
   $("<i>").addClass('fa fa-share fa').appendTo($span);
   $("<i>").addClass("fa fa-frown-o").appendTo($span);
-
   return $tweet;
-
 }
 
-$(document).ready(function() {
-var $tweet = createTweetElement(tweetData)
-console.log(createTweetElement(tweetData).prop("outerHTML"));
-$(".all-tweets").append($tweet)
+function renderTweets(tweets) {
+  $(".newTweet").empty()
+  for (var tweet of tweets) {
+    let $tweetHtml = createTweetElement(tweet);
+    $(".newTweet").append($tweetHtml)
+  }
+}
+
+function renderPage() {
+  $.get("/tweets", function (data) {
+    data.reverse()
+    renderTweets(data)
+  })
+};
+
+$(".inputField").on("submit",function(event) {
+   var $sendTweet = $(this);
+   event.preventDefault();
+   var $tweetMessage = $("textarea").val();
+   if ($tweetMessage.length > 0 && $tweetMessage.length <140){
+    var serializedMessage = $(this).serialize();
+    $.post("/tweets", serializedMessage, function() {
+      renderPage();
+    })
+  }else if ($tweetMessage.length > 140) {
+   alert("Please make your tweet shorter than 140 characters.")
+  }else if ($tweetMessage.length === 0) {
+   alert("Please enter a message.")
+  }
+});
+renderPage();
 })
+
+$(window).load(function(){
+   $(".new-tweet").hide()
+  })
+
+$(document).ready(function(){
+    $(".compose").click(function(){
+        $(".new-tweet").toggle(1000);
+    });
+ });
+
+  $(document).ready(function (){
+     $(".compose").click(function(){
+     $('.textInput').focus()
+    $('.textInput').select()
+});
+});
+
+
+
+
+
+
+
+
