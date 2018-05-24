9d9ef7d131e76e6515edf1e257e4d1e62f2f7178
Jeff <jeff.tang@live.com>
2018-05-24 14:12:52 -0400

    modified to accept mongo database

--- server/index.js
+++ server/index.js
@@ -7,30 +7,41 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
 
+const MongoClient = require("mongodb").MongoClient;
+const MONGODB_URI = "mongodb://localhost:27017/tweeter";
+
+
 app.use(bodyParser.urlencoded({
   extended: true
 }));
 app.use(express.static("public"));
 
 // The in-memory database of tweets. It's a basic object with an array in it.
-const db = require("./lib/in-memory-db");
+// const db = require("./lib/in-memory-db");
+MongoClient.connect(MONGODB_URI, (err, db) => {
+  if (err) {
+    console.error(`Failed to connect: ${MONGODB_URI}`);
+    throw err;
+  }
+  console.log(`Connected to mongodb: ${MONGODB_URI}`);
 
-// The `data-helpers` module provides an interface to the database of tweets.
-// This simple interface layer has a big benefit: we could switch out the
-// actual database it uses and see little to no changes elsewhere in the code
-// (hint hint).
-//
-// Because it exports a function that expects the `db` as a parameter, we can
-// require it and pass the `db` parameter immediately:
-const DataHelpers = require("./lib/data-helpers.js")(db);
+  // The `data-helpers` module provides an interface to the database of tweets.
+  // This simple interface layer has a big benefit: we could switch out the
+  // actual database it uses and see little to no changes elsewhere in the code
+  // (hint hint).
+  //
+  // Because it exports a function that expects the `db` as a parameter, we can
+  // require it and pass the `db` parameter immediately:
+  const DataHelpers = require("./lib/data-helpers.js")(db);
 
-// The `tweets-routes` module works similarly: we pass it the `DataHelpers` object
-// so it can define routes that use it to interact with the data layer.
-const tweetsRoutes = require("./routes/tweets")(DataHelpers);
+  // The `tweets-routes` module works similarly: we pass it the `DataHelpers` object
+  // so it can define routes that use it to interact with the data layer.
+  const tweetsRoutes = require("./routes/tweets")(DataHelpers);
 
-// Mount the tweets routes at the "/tweets" path prefix:
-app.use("/tweets", tweetsRoutes);
+  // Mount the tweets routes at the "/tweets" path prefix:
+  app.use("/tweets", tweetsRoutes);
 
-app.listen(PORT, () => {
-  console.log("Example app listening on port " + PORT);
-});
+  app.listen(PORT, () => {
+    console.log("Example app listening on port " + PORT);
+  });
+})
--- server/lib/data-helpers.js
+++ server/lib/data-helpers.js
@@ -8,7 +8,7 @@ module.exports = function makeDataHelpers(db) {
   return {
 
     // Saves a tweet to `db`
-    saveTweet: function(newTweet, callback) {
+    saveTweet: function (newTweet, callback) {
       simulateDelay(() => {
         db.tweets.push(newTweet);
         callback(null, true);
@@ -16,12 +16,10 @@ module.exports = function makeDataHelpers(db) {
     },
 
     // Get all tweets in `db`, sorted by newest first
-    getTweets: function(callback) {
-      simulateDelay(() => {
-        const sortNewestFirst = (a, b) => a.created_at - b.created_at;
-        callback(null, db.tweets.sort(sortNewestFirst));
-      });
-    }
+    getTweets: function (callback) {
+        db.collection("tweeter").find({}).toArray(callback)
+      
 
-  };
+    }
+  }
 }
