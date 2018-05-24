32a2386c342d245622c96a0b29adc8a8aba3421e
Jeff <jeff.tang@live.com>
2018-05-24 13:27:06 -0400

    getTweets function

--- mongo_example.js
+++ mongo_example.js
@@ -8,25 +8,32 @@ MongoClient.connect(MONGODB_URI, (err, db) => {
     console.error(`Failed to connect: ${MONGODB_URI}`);
     throw err;
   }
-
   // We have a connection to the "tweeter" db, starting here.
-  console.log(`Connected to mongodb: ${MONGODB_URI}`);
+  console.log(`Connetced to mongodb: ${MONGODB_URI}`);
 
-  // ==> Let's "get all the tweets". In Mongo-speak, we "find" them.
-  db.collection("tweets").find({}, (err, results) => {
-    if (err) throw err;
-
-    // ==> So we Read The Fantastic Manual, right?
-
-    // ==> We can iterate on the cursor to get results, one at a time:
-    console.log("for each item yielded by the cursor:");
-    // results.each((err, item) => console.log("  ", item));
-    results.toArray((err, resultsArray) => {
-      if (err) throw err;
-
-      console.log("results.toArray:", resultsArray);
+  // ==> Refactored and wrapped as new, tweet-specific function:
+  
+  function getTweets(callback) {
+    db.collection("tweeter").find().toArray((err, tweets) => {
+      if (err) {
+        return callback(err);
+      }
+      callback(null, tweets);
     });
-    // This is the end...
+  }
+
+  // ==> Later it can be invoked. Remember even if you pass
+  //     `getTweets` to another scope, it still has closure over
+  //     `db`, so it will still work. Yay!
+
+  getTweets((err, tweets) => {
+    if (err) throw err;
+    console.log("Logging each tweet:");
+    for (let tweet of tweets) {
+      console.log(tweet);
+    }
+
     db.close();
   });
+
 });
