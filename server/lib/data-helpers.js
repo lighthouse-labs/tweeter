a51ef415eb39bcc14e76bc200ef7c6f1ad1e42e2
Jeff <jeff.tang@live.com>
2018-05-24 14:42:06 -0400

    cleaned up code, removed initial tweet files

--- server/lib/data-helpers.js
+++ server/lib/data-helpers.js
@@ -7,13 +7,6 @@ const simulateDelay = require("./util/simulate-delay");
 module.exports = function makeDataHelpers(db) {
   return {
 
-    // // Saves a tweet to `db`
-    // saveTweet: function (newTweet, callback) {
-    //   simulateDelay(() => {
-    //     db.tweets.push(newTweet);
-    //     callback(null, true);
-    //   });
-    // },
     // Saves a tweet to `db`
     saveTweet: function (newTweet, callback) {
       simulateDelay(() => {
@@ -23,12 +16,6 @@ module.exports = function makeDataHelpers(db) {
     },
 
     // Get all tweets in `db`, sorted by newest first
-    // getTweets: function(callback) {
-    //   simulateDelay(() => {
-    //     const sortNewestFirst = (a, b) => a.created_at - b.created_at;
-    //     callback(null, db.tweets.sort(sortNewestFirst));
-    //   });
-    // }
     getTweets: function (callback) {
       simulateDelay(() => {
         db.collection("tweeter").find({}).sort({ created_at: 1 }).toArray(callback)
