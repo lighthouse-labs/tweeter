2b9134946b9ec0f0fb7d7d214fafacb8f2de7db7
Matt Greff <greff.matt@icloud.com>
2018-05-25 02:34:26 +0000

    switching to database to mongodb

--- server/lib/data-helpers.js
+++ server/lib/data-helpers.js
@@ -8,19 +8,16 @@ module.exports = function makeDataHelpers(db) {
   return {
 
     // Saves a tweet to `db`
-    saveTweet: function(newTweet, callback) {
-      simulateDelay(() => {
-        db.tweets.push(newTweet);
+    saveTweet: (newTweet, callback) => {
+        db.collection('tweets').insertOne(newTweet);
         callback(null, true);
-      });
     },
 
     // Get all tweets in `db`, sorted by newest first
-    getTweets: function(callback) {
-      simulateDelay(() => {
+    getTweets: (callback) => {
+        // db.collection('tweets').find().toArray(callback)
         const sortNewestFirst = (a, b) => a.created_at - b.created_at;
-        callback(null, db.tweets.sort(sortNewestFirst));
-      });
+        db.collection('tweets').find().toArray(callback);
     }
 
   };
