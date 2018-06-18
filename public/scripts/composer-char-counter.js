25cb5d3ed05e10fc11810d369d4176c4f2542e9d
leor8 <leo442183205@gmail.com>
2018-06-18 23:51:17 +0000

    getting class name to change character count is working partially

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,5 +1,8 @@
 $(document).ready(function() {
-  $("#newTweet").on("keypress", function() {
-    console.log(this);
+  let charCount = 140;
+  $("#newTweet").on("keyup", function(event) {
+    let updateTag = $(this).parent().children(".counter");
+    console.log(document.getElementsByClassName(updateTag).innerHTML);
+    document.getElementsByClassName(updateTag).innerHTML = charCount - event.target.value.length;
   });
 });
