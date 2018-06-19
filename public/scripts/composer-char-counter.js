4e50a6d14f19823d6cb767715d8ccc0db22b0bbb
leor8 <leo442183205@gmail.com>
2018-06-19 17:01:11 +0000

    drawing tweet interface

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -2,13 +2,12 @@ $(document).ready(function() {
   let charCount = 140;
   $("#newTweet").on("keyup", function(event) {
     let updateTag = $(this).parent().children(".counter")[0];
-    console.log(updateTag);
-    //console.log(document.getElementsByClassName(updateTag).textContent);
     updateTag.textContent = charCount - Array.from(event.target.value).length;
 
     if(charCount - Array.from(event.target.value).length < 0){
       $(updateTag).css("color", "red");
+    } else {
+      $(updateTag).css("color", "black");
     }
-
   });
 });
