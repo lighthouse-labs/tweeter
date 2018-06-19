d84419fc47eba40be71435c7d62fd473a2f1c626
Eliza Moore <jdlmmoore@Elizas-MacBook-Air.local>
2018-06-19 14:20:25 -0400

    correct counter to account or backspaces

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -2,9 +2,14 @@ $(document).ready(function() {
   let counter = 140;
 
   $(".new-tweet form #tweetBox").keydown(function(event) {
-    let textLength = $(this).val().length;
+    let textLength = 0;
+    if(event.key === "Backspace"){
+      textLength = $(this).val().length - 1;
+    } else {
+      textLength = $(this).val().length + 1;
+    }
     console.log($(this).val().length);
-    $('.counter').text(140 - textLength);
+    $(this).parent().children('span').text(140 - textLength);
   })
 
 
