b7e78b1933085b03bece00e2610bcb0f3e381fdf
Eliza Moore <jdlmmoore@Elizas-MacBook-Air.local>
2018-06-19 16:46:46 -0400

    better eventlistener on counter function

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -2,26 +2,11 @@ $(document).ready(function() {
 
   //change character count when a key is pressed in the tweet text area
 
-  $(".new-tweet form #tweetBox").keydown(function(event) {
-    let textLength = 0;
+  $(".new-tweet form #tweetBox").keyup(function(event) {
     let newCount;
-
-    //if the key is backspace, text length is minus 1 (unless length is 0)
-
-    if(event.key === "Backspace"){
-      textLength = $(this)
+    let textLength = $(this)
                     .val()
-                    .length - 1;
-
-      if(textLength < 0){
-        textLength = 0;
-      }
-    } else {
-
-      textLength = $(this)
-                    .val()
-                    .length + 1;
-    }
+                    .length;
 
     //update value of the counter
 
