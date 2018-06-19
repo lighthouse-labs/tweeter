06419613576587f3d298f1e02342c4a64aad04d6
Eliza Moore <jdlmmoore@Elizas-MacBook-Air.local>
2018-06-19 14:51:32 -0400

    added response.css page for real-time changes, added counter change to red if past 140 chars

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -3,13 +3,33 @@ $(document).ready(function() {
 
   $(".new-tweet form #tweetBox").keydown(function(event) {
     let textLength = 0;
+    let newCount;
+
     if(event.key === "Backspace"){
       textLength = $(this).val().length - 1;
+      if(textLength < 0){
+        textLength = 0;
+      }
     } else {
       textLength = $(this).val().length + 1;
     }
     console.log($(this).val().length);
-    $(this).parent().children('span').text(140 - textLength);
+
+    if(textLength === 0){
+      newCount = 140;
+    } else {
+    newCount = 140 - textLength;
+    }
+    let element = $(this).parent().children('.counter');
+
+    element.text(newCount);
+
+    if(newCount < 0){
+      console.log("HERE")
+      element.addClass('overLimit');
+    } else {
+      element.removeClass('overLimit');
+    }
   })
 
 
