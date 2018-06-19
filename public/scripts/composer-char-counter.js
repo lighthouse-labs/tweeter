92cf43d0c700ec46507b4c47438804ded0cb7fb8
leor8 <leo442183205@gmail.com>
2018-06-19 00:32:03 +0000

    character counting dynamically functional

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,8 +1,14 @@
 $(document).ready(function() {
   let charCount = 140;
   $("#newTweet").on("keyup", function(event) {
-    let updateTag = $(this).parent().children(".counter");
-    console.log(document.getElementsByClassName(updateTag).innerHTML);
-    document.getElementsByClassName(updateTag).innerHTML = charCount - event.target.value.length;
+    let updateTag = $(this).parent().children(".counter")[0];
+    console.log(updateTag);
+    //console.log(document.getElementsByClassName(updateTag).textContent);
+    updateTag.textContent = charCount - Array.from(event.target.value).length;
+
+    if(charCount - Array.from(event.target.value).length < 0){
+      $(updateTag).css("color", "red");
+    }
+
   });
 });
