e97b6027eab7d4997ea42d2545c2c241b3ddc4d4
Tiffany <tiffanyjdow@gmail.com>
2018-06-19 19:06:43 +0000

    Added counter functionality and got counter to change to red when registering negative values

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+
+  var charMax = 140;
+
+  $('textarea').on('keyup', function() {
+    var textLength = $(this).val().length;
+    var remainingText = charMax - textLength;
+
+    var textCounter = $(this).siblings('.counter').text(remainingText);
+
+    if (remainingText < 0) {
+      $('.new-tweet span').addClass('color');
+    } else if (remainingText > 0) {
+      $('.new-tweet span').removeClass('color');
+    }
+  });
+
+});
