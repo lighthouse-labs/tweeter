2a7df212c164d2f3cb8ad793d32c839946f15f32
emmarskillings <emmarskillings@gmail.com>
2018-06-19 20:56:42 +0000

    counter now works and goes red when more than 140 characters in text box

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+
+  const maxCharacter = 140;
+
+  $(".new-tweet form textarea").on("keyup", function(event) {
+
+    let textLength = $(this).val().length;
+    let counter = maxCharacter - textLength;
+    let counterElement = $(this).siblings(".counter");
+
+    counter < 0 ? $(counterElement).text(counter).css("color", "red") : $(counterElement).text(counter).css("color", "black");
+
+  });
+});
+
