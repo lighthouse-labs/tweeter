ad340f910a3dab8ae417f8cb85790355b831f18d
camueljackson <camille.a.pelletier@gmail.com>
2018-06-19 22:33:15 +0000

    counter working, historic tweet section mostly formatted

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -3,19 +3,23 @@ $(document).ready(function() {
 
   let charCount = 10;
 
-  $('textarea').on('keyup', function () {
+  $('textarea').on('keyup', function (event) {
 
     let charAmount = $(this).val().length;
 
     let counter = charCount - charAmount;
 
-    let textCount = $(this).siblings('.counter').text(counter);
+    $(this).siblings('.counter').text(counter);
 
     if (counter <= 0) {
+
       $('.new-tweet span').addClass('redCounter');
+
     } else {
+
       $('.new-tweet span').removeClass('redCounter');
-    }
+
+    } // END OF IF/ELSE STATEMENT
 
   }); // END OF $('.textarea') KEYUP FUNCTION
 
