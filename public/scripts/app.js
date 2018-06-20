31d9210ecca41b5221c06218132eb5a625b3747e
emmarskillings <emmarskillings@gmail.com>
2018-06-20 20:11:43 +0000

    tweets now post automatically without refreshing and alerts pop up if text box is empty or contains more than 140 characters

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -6,92 +6,41 @@
 
 $(document).ready(function() {
 
+  const frown = `<i class="far fa-frown"></i>`
+  const cringe = `<i class="far fa-meh"></i>`
 
-function loadTweets() {
-  $.ajax ({
-    type: 'GET',
-    url: '/tweets',
-    success: function(data) {
-      renderTweets(data);
+  function loadTweets() {
+    $.ajax ({
+      type: 'GET',
+      url: '/tweets',
+      success: renderTweets
+    });
+  }
+
+
+
+    $("form").on( "submit", function(data) {
+      event.preventDefault();
+      if ($('textarea').val().length === 0) {
+        $(".alert").append(`Hey! You gotta type something before you can tweet it ${frown}`).fadeOut(7000);
+      } else if ($('textarea').val().length > 140) {
+        $(".alert").append(`Uh oh... looks like you've got too much to say ${cringe}`).fadeOut(7000)
+      } else {
+        $.ajax ({
+        type: 'POST',
+        url: '/tweets',
+        data: $(this).serialize(),
+        success: function(data) {
+          loadTweets();
+          $('textarea').val('');
+          //$(".alert").append(`Tweet Tweet!`).animate({color: 'green'});
+        }
+      });
     }
   });
-}
 
 
-
-
-$("form").on( "submit", function(data) {
-  event.preventDefault();
-  $.ajax ({
-    type: 'POST',
-    url: '/tweets',
-    data: $(this).serialize(),
-    success: function(data) {
-      renderTweets(data);
-    }
-  });
-});
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const data = [
-//   {
-//     "user": {
-//       "name": "Newton",
-//       "avatars": {
-//         "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
-//         "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
-//         "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
-//       },
-//       "handle": "@SirIsaac"
-//     },
-//     "content": {
-//       "text": "If I have seen further it is by standing on the shoulders of giants"
-//     },
-//     "created_at": 1461116232227
-//   },
-//   {
-//     "user": {
-//       "name": "Descartes",
-//       "avatars": {
-//         "small":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
-//         "regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
-//         "large":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
-//       },
-//       "handle": "@rd" },
-//     "content": {
-//       "text": "Je pense , donc je suis"
-//     },
-//     "created_at": 1461113959088
-//   },
-//   {
-//     "user": {
-//       "name": "Johann von Goethe",
-//       "avatars": {
-//         "small":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
-//         "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
-//         "large":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
-//       },
-//       "handle": "@johann49"
-//     },
-//     "content": {
-//       "text": "Es ist nichts schrecklicher als eine tätige Unwissenheit."
-//     },
-//     "created_at": 1461113796368
-//   }
-// ];
-
-function createTweetElement(tweet) {
+  function createTweetElement(tweet) {
 
     const icon = tweet['user']['avatars']['small'];
 
@@ -122,18 +71,17 @@ function createTweetElement(tweet) {
                       </footer>
                   </article>`
 
-
-
-    //$('#tweets-container').append(article);
     return article;
   };
 
   function renderTweets(tweets) {
+    $('#tweets-container').empty();
     for (tweet of tweets) {
       $('#tweets-container').prepend(createTweetElement(tweet));
     }
   };
 
   loadTweets();
+
 
 });
