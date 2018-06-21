5f3007c871694bce15f50f9effc85e24455d0a40
leor8 <leo442183205@gmail.com>
2018-06-21 20:54:34 +0000

    switching to mongodb

--- server/index.js
+++ server/index.js
@@ -11,30 +11,35 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 // The in-memory database of tweets. It's a basic object with an array in it.
-const db = require("./lib/in-memory-db");
+//const db = require("./lib/in-memory-db");
+const { MongoClient } = require("mongodb");
+const MONGODB_URI = "mongodb://localhost:27017/tweeter";
 
-// The `data-helpers` module provides an interface to the database of tweets.
-// This simple interface layer has a big benefit: we could switch out the
-// actual database it uses and see little to no changes elsewhere in the code
-// (hint hint).
-//
-// Because it exports a function that expects the `db` as a parameter, we can
-// require it and pass the `db` parameter immediately:
-const DataHelpers = require("./lib/data-helpers.js")(db);
+MongoClient.connect(MONGODB_URI, (err, db) => {
+  if(err){
+    console.error("You have encountered an error when you try to connect to MongoDB");
+  }
+  // The `data-helpers` module provides an interface to the database of tweets.
+  // This simple interface layer has a big benefit: we could switch out the
+  // actual database it uses and see little to no changes elsewhere in the code
+  // (hint hint).
+  //
+  // Because it exports a function that expects the `db` as a parameter, we can
+  // require it and pass the `db` parameter immediately:
+  const DataHelpers = require("./lib/data-helpers.js")(db);
 
-// The `tweets-routes` module works similarly: we pass it the `DataHelpers` object
-// so it can define routes that use it to interact with the data layer.
-const tweetsRoutes = require("./routes/tweets")(DataHelpers);
+  // The `tweets-routes` module works similarly: we pass it the `DataHelpers` object
+  // so it can define routes that use it to interact with the data layer.
+  const tweetsRoutes = require("./routes/tweets")(DataHelpers);
 
-// Mount the tweets routes at the "/tweets" path prefix:
-app.use("/tweets", tweetsRoutes);
+  // Mount the tweets routes at the "/tweets" path prefix:
+  app.use("/tweets", tweetsRoutes);
 
-app.listen(PORT, () => {
-  console.log("Example app listening on port " + PORT);
+  app.listen(PORT, () => {
+    console.log("Example app listening on port " + PORT);
+  });
+
 });
-
-
-
 
 
 
--- server/lib/data-helpers.js
+++ server/lib/data-helpers.js
@@ -9,17 +9,29 @@ module.exports = function makeDataHelpers(db) {
 
     // Saves a tweet to `db`
     saveTweet: function(newTweet, callback) {
-      simulateDelay(() => {
-        db.tweets.push(newTweet);
-        callback(null, true);
-      });
+      // simulateDelay(() => {
+      //   db.tweets.push(newTweet);
+      //   callback(null, true);
+      // });
+
+      db.collection("tweets").insertOne(newTweet);
+      callback(null, true);
     },
 
     // Get all tweets in `db`, sorted by newest first
     getTweets: function(callback) {
-      simulateDelay(() => {
-        const sortNewestFirst = (a, b) => a.created_at - b.created_at;
-        callback(null, db.tweets.sort(sortNewestFirst));
+      // simulateDelay(() => {
+      //   const sortNewestFirst = (a, b) => a.created_at - b.created_at;
+      //   callback(null, db.tweets.sort(sortNewestFirst));
+      // });
+
+      db.collection("tweets").find().toArray((err, tweets) => {
+        if(err) {
+          return callback(err);
+        }
+
+        callback(null, tweets);
+
       });
     }
 
