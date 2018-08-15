18a0871d0fe5b072af6a9c9b6b33f81f594a2ef0
Kyla Palos <kyla.palos@gmail.com>
2018-08-15 14:40:51 +0000

    saving d2 progress

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -3,4 +3,104 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
+$(document).ready(function() {
+  const data = [
+    {
+      "user": {
+        "name": "Newton",
+        "avatars": {
+          "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
+          "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
+          "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
+        },
+        "handle": "@SirIsaac"
+      },
+      "content": {
+        "text": "If I have seen further it is by standing on the shoulders of giants"
+      },
+      "created_at": 1461116232227
+    },
+    {
+      "user": {
+        "name": "Descartes",
+        "avatars": {
+          "small":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
+          "regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
+          "large":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
+        },
+        "handle": "@rd" },
+      "content": {
+        "text": "Je pense , donc je suis"
+      },
+      "created_at": 1461113959088
+    },
+    {
+      "user": {
+        "name": "Johann von Goethe",
+        "avatars": {
+          "small":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
+          "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
+          "large":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
+        },
+        "handle": "@johann49"
+      },
+      "content": {
+        "text": "Es ist nichts schrecklicher als eine tätige Unwissenheit."
+      },
+      "created_at": 1461113796368
+    }
+  ];
+
+
+  function calculateDateStamp(milliseconds) {
+    let today = new Date().getTime();
+    let timeDifference = today - new Date(milliseconds).getTime();
+
+    let days, total_hours, total_minutes, total_seconds;
+
+    total_seconds = parseInt(Math.floor(timeDifference / 1000));
+    total_minutes = parseInt(Math.floor(total_seconds / 60));
+    total_hours = parseInt(Math.floor(total_minutes / 60));
+    days = parseInt(Math.floor(total_hours / 24));
+
+    return days;
+
+  }
+
+  function createTweetElement(tweetData){
+    let dateDifference = calculateDateStamp(tweetData.created_at);
+
+    let element = `<article class="tweet">
+            <header>
+              <img src="${tweetData.user.avatars.small}">
+              <h2>${tweetData.user.name}</h2>
+              <p>${tweetData.user.handle}</p>
+            </header>
+            <p>${tweetData.content.text}</p>
+            <footer>
+              <p>${dateDifference} days ago</p>
+              <div class="icons">
+                <i class="fas fa-flag"></i>
+                <i class="fas fa-retweet"></i>
+                <i class="fas fa-heart"></i>
+              </div>
+            </footer>
+          </article>`;
+    return element;
+
+  }
+
+  function renderTweets(data){
+    data.forEach((item) => {
+      let $tweet = createTweetElement(item);
+      $(".tweets-container").append($tweet);
+    });
+  }
+
+  renderTweets(data);
+  console.log("app.js ran");
+
+});
+
+
 
---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,18 @@
+$(document).ready(() => {
+
+  $("textarea").keyup(function(){
+
+   let counter = 140 - $(this).val().length;
+
+   let $counterNode = $(this).siblings(".counter");
+   $counterNode.text(counter);
+   if(counter < 0){
+    $counterNode.css("color", "#ff0000");
+   } else {
+    $counterNode.css("color", "#000000");
+   }
+
+  });
+
+});
+
