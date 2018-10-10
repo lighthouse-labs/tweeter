aea46373f5cd62070882089e37360675a6196436
MPriest <miche.priest@gmail.com>
2018-10-10 16:00:55 -0700

    created container for old tweets

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,3 +4,23 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+const tweetData = {
+  "user": {
+    "name": "Newton",
+    "avatars": {
+      "small": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
+      "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
+      "large": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
+    },
+    "handle": "@SirIsaac"
+  },
+  "content": {
+    "text": "If I have seen further it is by standing on the shoulders of giants"
+  },
+  "created_at": 1461116232227
+}
+
+var $tweet = createTweetElement(tweetData);
+
+console.log($tweet);
+$tweet('#tweets-container').append($tweet);
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -3,6 +3,11 @@ $(document).ready(function() {
   $( ".text-area" ).on("input", function() {
     let charLength = 140 - ($(this).val().length);
     $(".counter").text(charLength);
+    if (charLength < 0) {
+      $(".counter").addClass("counter-negative"); // don't need . if not a selector
+    } else {
+      $(".counter").removeClass("counter-negative");
+    }
   });
 
 });
