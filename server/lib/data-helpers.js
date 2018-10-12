876b2ead0eb37a16c075676655cfff3bf25e2257
michepriest <miche.priest@gmail.com>
2018-10-12 22:34:23 +0000

    comments to identify 'factory functions'

--- server/lib/data-helpers.js
+++ server/lib/data-helpers.js
@@ -4,7 +4,7 @@
 const simulateDelay = require("./util/simulate-delay");
 
 // Defines helper functions for saving and getting tweets, using the database `db`
-module.exports = function makeDataHelpers(db) {
+module.exports = function makeDataHelpers(db) {  // a "factory function", a function that creates an object and returns it
   return {
 
     // Saves a tweet to `db`
--- server/routes/tweets.js
+++ server/routes/tweets.js
@@ -5,7 +5,7 @@ const userHelper    = require("../lib/util/user-helper")
 const express       = require('express');
 const tweetsRoutes  = express.Router();
 
-module.exports = function(DataHelpers) {
+module.exports = function(DataHelpers) {  // a "factory function", a function that creates an object and returns it
 
   tweetsRoutes.get("/", function(req, res) {
     DataHelpers.getTweets((err, tweets) => {
