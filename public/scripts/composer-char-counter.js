52928e46e4b2fcc4a2bd0dc5838d4f8879543c0b
danvangogh <djameswhite@yahoo.com>
2018-11-07 04:47:40 +0000

    added initial hover feature - no icons yet

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,7 +1,27 @@
 $(document).ready(function() {
-  console.log("noiiice");
 
-  $('textarea').on("paste keydown", function(event) {
-    console.log(140 - event.target.textLength);
+  $('#new-tweet-form').on("input", function(event) {
+
+    const target = $(event.currentTarget);
+    const currentLength = target.find('textarea').val().length;
+    const characterCounter = target.find('.counter');
+    const maxLength = 140;
+
+    let count = maxLength - event.target.textLength;
+
+      console.log(characterCounter);
+      console.log(target);
+
+    characterCounter.html(count);
+
+    if (count < 0) {
+      console.log("zaeroo")
+      characterCounter.css("color", "red");
+    } else {
+      characterCounter.css("color", "#334853")
+    }
   });
 });
+
+
+// $('div').css('background-color', 'red').css('font-size', 6rem');
