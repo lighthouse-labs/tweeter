02cdc06bdc1e7924c5ca59011893cafe7387883d
Mark Barlescu <barlescu92@gmail.com>
2018-11-07 20:53:09 +0000

    able to make another box for new tweets

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,6 +1,27 @@
-/*
- * Client-side JS logic goes here
- * jQuery is already loaded
- * Reminder: Use (and do all your DOM work in) jQuery's document ready function
- */
+const tweetData = {
+  "user": {
+    "name": "Newton",
+    "avatars": {
+      "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
+      "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
+      "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
+    },
+    "handle": "@SirIsaac"
+  },
+  "content": {
+    "text": "If I have seen further it is by standing on the shoulders of giants"
+  },
+  "created_at": 1461116232227
+}
 
+function createTweetElement (data) {
+  let $tweet = $("<article>").addClass("tweet");
+  $tweet.text(data.content.text);
+  return $tweet;
+}
+
+let $tweet = createTweetElement(tweetData);
+
+// Test / driver code (temporary)
+console.log($tweet); // to see what it looks like
+$('#tweet-container').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
