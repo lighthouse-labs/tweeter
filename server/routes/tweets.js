7b41640cfce9ba9f429a495e727dcbfff2b271b5
Matt Kelly <matt.r.kelly27@gmail.com>
2019-01-22 04:06:45 +0000

    tweeter for Monday finished

--- server/routes/tweets.js
+++ server/routes/tweets.js
@@ -40,7 +40,7 @@ module.exports = function(DataHelpers) {
       if (err) {
         res.status(500).json({ error: err.message });
       } else {
-        res.status(201).send();
+        res.status(200).send();
       }
     });
   });
