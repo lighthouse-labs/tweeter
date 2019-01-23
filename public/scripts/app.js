769c8fa3ee674606c5cf448617a2847d247553b0
marcgregi <marc@hypedigital.co>
2019-01-23 15:50:20 +0000

    Adding dynamic tweet functionality to the main timeline container

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,3 +4,90 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+ // Test / driver code (temporary). Eventually will get this from the server.
+ const data = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": {
+        "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
+        "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
+        "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
+      },
+      "handle": "@SirIsaac"
+    },
+    "content": {
+      "text": "How did the hipster burn his tongue? He drank his coffee before it was cool"
+    },
+    "created_at": 1461116232227
+  },
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": {
+        "small":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
+        "regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
+        "large":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
+      },
+      "handle": "@rd" },
+    "content": {
+      "text": "What did the mom coffee bean say to the kid coffee bean? You're grounded."
+    },
+    "created_at": 1461113959088
+  },
+  {
+    "user": {
+      "name": "Johann von Goethe",
+      "avatars": {
+        "small":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
+        "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
+        "large":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
+      },
+      "handle": "@johann49"
+    },
+    "content": {
+      "text": "Hey, wanna hear a joke about ghosts?<br/><br/> That's the spirit"
+    },
+    "created_at": 1461113796368
+  }
+];
+
+
+document.addEventListener('DOMContentLoaded', function(event) {
+
+  function createTweetElement(data) {
+    let date = new Date(data.created_at * 1000);
+    let $article = $('<article>').addClass('tweet');
+
+    $article.html(`
+      <header>
+        <img src=${data.user.avatars.small} alt=${data.user.name}>
+        <h2>${data.user.name}</h2>
+        <p>${data.user.handle}</p>
+      </header>
+      <div class="tweet-body">
+        <p>${data.content.text}</p>
+      </div>
+      <footer>
+        <p>${date.toLocaleDateString("en-US")}</p>
+        <div class="footer-icons">
+          <img src="https://png.icons8.com/windows/64/000000/flag.png" alt="flag">
+          <img src="https://png.icons8.com/windows/64/000000/available-updates.png">
+          <img src="https://png.icons8.com/windows/64/000000/hearts.png">
+        </div>
+      </footer>
+      `);
+    return $article;
+  };
+
+  function renderTweets(data) {
+    for (let tweets in data) {
+      let tweet = data[tweets];
+      let $tweet = createTweetElement(tweet);
+      $('#timeline').append($tweet);
+    }
+  };
+
+  renderTweets(data);
+});
+
