91687b226de2acc76194bd8f264ef932b8d8713a
ZHShang <zhihengshang@hotmail.com>
2019-05-14 17:21:49 +0000

    Fixed the counter so it works properly while holding down the delete key

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,12 +1,13 @@
 $(document).ready(function(){
-  $('#txt').on('keyup', function(){
+  $('#txt').on('input', function(){
     var totalChars = $(this).val().length;
     var charLeft = 140;
     charLeft = charLeft - totalChars;
-    //$('.counter').html(charLeft);
     $(this).parent().find('span').html(charLeft);
     if(charLeft < 0){
       $(this).parent().find('span').css("color", "red");
+    } else {
+      $(this).parent().find('span').css("color", "black");
     }
   });
 });
