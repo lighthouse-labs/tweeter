93215a36be0cbc4f13948d0553c0c166e2d527d2
ZHShang <zhihengshang@hotmail.com>
2019-05-14 23:28:55 +0000

    finished up the loop to dynamically add tweets

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,6 +4,53 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+const data = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": {
+        "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
+        "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
+        "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
+      },
+      "handle": "@SirIsaac"
+    },
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 1461116232227
+  },
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": {
+        "small":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
+        "regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
+        "large":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
+      },
+      "handle": "@rd" },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+  },
+  {
+    "user": {
+      "name": "Johann von Goethe",
+      "avatars": {
+        "small":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
+        "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
+        "large":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
+      },
+      "handle": "@johann49"
+    },
+    "content": {
+      "text": "Es ist nichts schrecklicher als eine tätige Unwissenheit."
+    },
+    "created_at": 1461113796368
+  }
+];
+
 $(document).ready(function(){
 
 function createTweetElement(tweetObj){
@@ -12,8 +59,7 @@ function createTweetElement(tweetObj){
   const $userName = $('<h3>').text(tweetObj.user.name);
   $header.append($userName);
   $article.append($header);
-  //$('<article>' + tweetObj.content.text + '</article>').appendTo('section.tweets');
-  const $temptTweet=
+  const $tempTweet=
   `<article class="tweet">
       <header>
         <img class="userPic" src="" align="bottom">
@@ -24,33 +70,15 @@ function createTweetElement(tweetObj){
       <footer>1 day ago</footer>
     </article>
   `;
-  $('.tweets').append($temptTweet);
-  console.log($userName);
+  return $tempTweet;
 
 }
 
-
-// Test / driver code (temporary). Eventually will get this from the server.
-const tweetData = {
-  "user": {
-    "name": "Newton",
-    "avatars": {
-      "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
-      "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
-      "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
-    },
-    "handle": "@SirIsaac"
-  },
-  "content": {
-    "text": "If I have seen further it is by standing on the shoulders of giants"
-  },
-  "created_at": 1461116232227
+function renderTweets(arrTweets){
+  arrTweets.forEach(function(e){
+    $('.tweets').append(createTweetElement(e));
+  });
 }
 
-var $tweet = createTweetElement(tweetData);
-
-// Test / driver code (temporary)
-console.log($tweet); // to see what it looks like
-// to add it to the page so we can make sure it's got all the right elements, classes, etc.
-
+renderTweets(data);
 });
