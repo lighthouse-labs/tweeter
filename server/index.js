3972ff067f3178b5d9961d373382566c89e07962
Aurlin Dhillon <aurlindhillon@gmail.com>
2020-03-16 12:41:20 -0700

    testing github

--- server/index.js
+++ server/index.js
@@ -1,11 +1,12 @@
 "use strict";
 
 // Basic express setup:
+//ss
 
-const PORT          = 8080;
-const express       = require("express");
-const bodyParser    = require("body-parser");
-const app           = express();
+const PORT = 8080;
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
