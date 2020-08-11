2af4f12fbac6abce781f0e1a7b288dce95fb069c
Jeffrey Shao <shaoyuhao970909@gmail.com>
2020-08-11 15:54:03 +0000

    form indentation is fixed

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,14 +1,14 @@
 $(document).ready(function () {
   $('.new-tweet form textarea').on('keydown', function (e) {
 
-     ($(this).val().length>140)? 
-      $('.new-tweet div .counter').css('color','red') : $('.new-tweet div .counter').css('color','#545149');
-    
-    if($('.new-tweet div .counter').val() === '140' && e.which === 8){
+    ($(this).val().length > 140) ?
+      $('.new-tweet div .counter').css('color', 'red') : $('.new-tweet div .counter').css('color', '#545149');
+
+    if ($('.new-tweet div .counter').val() === '140' && e.which === 8) {
       $('.new-tweet div .counter').val('140');
       return;
     }
-    
+
     if (e.which === 8) {
       $('.new-tweet div .counter').val(parseInt($('.new-tweet div .counter').val()) + 1);
     } else {
