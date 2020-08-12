6222a537c74115ae2a1912d47485008031fc9018
Jeffrey Shao <shaoyuhao970909@gmail.com>
2020-08-12 16:21:48 +0000

    fixed issue for hovrting effects for all tweets

--- public/scripts/hover-effect.js
+++ public/scripts/hover-effect.js
@@ -1,11 +1,11 @@
 $(document).ready(function(){
-  $('#tweet-container article').on('mouseover', function(){
+  $('#tweet-container').on('mouseover','.tweet', function(){
     $(this).addClass('change');
     $(this).css('opacity','100%');
     $(this).css('border-color','black');
     $('#tweet-container article header .handle').css('display','inline');
   })
-  $('#tweet-container article').on('mouseleave', function(){
+  $('#tweet-container').on('mouseleave','.tweet', function(){
     $(this).removeClass('change');
     $(this).css('opacity','70%');
     $(this).css('border-color','rgb(104, 101, 101)');
