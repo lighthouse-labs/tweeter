c88fdcb398a31b97b45fe55b94a1d0458b32be02
Jeffrey Shao <shaoyuhao970909@gmail.com>
2020-08-12 05:29:43 +0000

    added three function inside jquery, when the document is loaded, these three function execute, pass-in an array of data as arguments, eventually render user tweets  on main pages.

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,3 +4,76 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+$(document).ready(function(){
+  const calculateTimeElapsed = function(time){
+    const oneDay = 24 * 60 * 60 *1000;
+    const timeDiffInmili = Date.now() - time;
+    const timeDiffInDay = timeDiffInmili /oneDay;
+    // if(timeDiffInDay >= 365){
+    //  timeDiffInDay /= 365;
+    // }else{
+    //   timeDiffInDay;
+    // }
+    return timeDiffInDay.toFixed(0);
+  };
+
+  const createTweetElement = function (tweet) {
+    let $tweet = $(`<article class="tweet">
+                  <header>
+                    <span class='avator'><img src= ${tweet['user']["avatars"]}>
+                    <span>${tweet['user']['name']}</span>
+                    </span>
+                    <span class='handle'>${tweet['user']['handle']}</span>
+                  </header>
+                  <div class='content'>${tweet['content']['text']}</div>
+                  <footer>
+                    <span> ${calculateTimeElapsed(tweet["created_at"])} days ago</span>
+                    <div class="symbol">
+                      <div class="icon star">&starf;</div>
+                      <div class="icon arrow">&#8633;</div>
+                      <div class="icon heart">&#9829;</div>
+                    </div>
+                  </footer>
+                 </article>`);
+     return $tweet;
+  }
+
+  const renderTweets = function(tweets) {
+    // loops through tweets
+    // calls createTweetElement for each tweet
+    // takes return value and appends it to the tweets container
+   for(const tweet of tweets){
+     let renderedElem = createTweetElement(tweet);
+     $('#tweet-container').append(renderedElem);
+     $('#tweet-container').append(`<br>`)
+   }
+  }
+
+  const data = [
+    {
+      "user": {
+        "name": "Newton",
+        "avatars": "https://i.imgur.com/73hZDYK.png"
+        ,
+        "handle": "@SirIsaac"
+      },
+      "content": {
+        "text": "If I have seen further it is by standing on the shoulders of giants"
+      },
+      "created_at": 1461116232227
+    },
+    {
+      "user": {
+        "name": "Descartes",
+        "avatars": "https://i.imgur.com/nlhLi3I.png",
+        "handle": "@rd" },
+      "content": {
+        "text": "Je pense , donc je suis"
+      },
+      "created_at": 1461113959088
+    }
+  ];
+
+  renderTweets(data);
+});
+
