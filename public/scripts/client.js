9eae35c9a4c50ac74e8ccdd84d97ac51aa8d6c7a
Jeffrey Shao <shaoyuhao970909@gmail.com>
2020-08-12 22:53:02 +0000

    empty the original tweet container before rendering the new container including the recently added tweet

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,8 +3,6 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
-
-$(document).ready(function(){
   
   const calculateTimeElapsed = function(time){
     const oneDay = 24 * 60 * 60 *1000;
@@ -35,7 +33,8 @@ $(document).ready(function(){
                       <div class="icon heart">&#9829;</div>
                     </div>
                   </footer>
-                 </article>`);
+                 </article>
+                 <br/>`);
      return $tweet;
   }
 
@@ -43,63 +42,42 @@ $(document).ready(function(){
     // loops through tweets
     // calls createTweetElement for each tweet
     // takes return value and appends it to the tweets container
-   for(const tweet of tweets){
+   for(let tweet of tweets){
      let renderedElem = createTweetElement(tweet);
-     $('#tweet-container').append(renderedElem);
-     $('#tweet-container').append(`<br>`)
+     $('#tweet-container').prepend(renderedElem);
    }
   }
-  
-  const data = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png"
-        ,
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd" },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    }
-  ];
-  
-  // renderTweets(data);
 
+
+  $(document).ready(function(){
+  
   const $form = $('.container .new-tweet form');
-  $form.on('submit', (event)=>{
-    event.preventDefault();
-    const serialized = $form.serialize();
-    console.log(serialized);
-    
-     $.post('/tweets', serialized)
-      .then(()=>{
-        console.log('successful!!!')
-        loadtweets();
-      })
-  })
 
   const loadtweets = function(){
     $.getJSON('/tweets')
     .then((responseData)=>{
       console.log('the fetching is successful');
-      // $('#tweet-container').html();
+      $('#tweet-container').empty();
       renderTweets(responseData);
-
     })
   }
+
   loadtweets();
+
+  $form.submit((event) => {
+    console.log("here");
+    event.preventDefault();
+    
+    const serialized = $form.serialize();
+    $.post('/tweets', serialized)
+    .then(()=>{
+      console.log('successful!!!')
+        loadtweets();
+    })
+  })
+
+  
+  //  loadtweets();
 
 });
 
