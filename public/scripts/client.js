70b15b81fbe42c9208dd574faf590292b53275b6
Jeffrey Shao <shaoyuhao970909@gmail.com>
2020-08-12 23:18:35 +0000

    add a if else statement if textarea input is empty or it exceed word limit

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,21 +3,21 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
-  
-  const calculateTimeElapsed = function(time){
-    const oneDay = 24 * 60 * 60 *1000;
-    const timeDiffInmili = Date.now() - time;
-    const timeDiffInDay = timeDiffInmili /oneDay;
-    // if(timeDiffInDay >= 365){
-    //  timeDiffInDay /= 365;
-    // }else{
-    //   timeDiffInDay;
-    // }
-    return timeDiffInDay.toFixed(0);
-  };
 
-  const createTweetElement = function (tweet) {
-    let $tweet = $(`<article class="tweet">
+const calculateTimeElapsed = function (time) {
+  const oneDay = 24 * 60 * 60 * 1000;
+  const timeDiffInmili = Date.now() - time;
+  const timeDiffInDay = timeDiffInmili / oneDay;
+  // if(timeDiffInDay >= 365){
+  //  timeDiffInDay /= 365;
+  // }else{
+  //   timeDiffInDay;
+  // }
+  return timeDiffInDay.toFixed(0);
+};
+
+const createTweetElement = function (tweet) {
+  let $tweet = $(`<article class="tweet">
                   <header>
                     <span class='avator'><img src= ${tweet['user']["avatars"]}>
                     <span>${tweet['user']['name']}</span>
@@ -35,48 +35,52 @@
                   </footer>
                  </article>
                  <br/>`);
-     return $tweet;
+  return $tweet;
+}
+
+const renderTweets = function (tweets) {
+  // loops through tweets
+  // calls createTweetElement for each tweet
+  // takes return value and appends it to the tweets container
+  for (let tweet of tweets) {
+    let renderedElem = createTweetElement(tweet);
+    $('#tweet-container').prepend(renderedElem);
   }
-
-  const renderTweets = function(tweets) {
-    // loops through tweets
-    // calls createTweetElement for each tweet
-    // takes return value and appends it to the tweets container
-   for(let tweet of tweets){
-     let renderedElem = createTweetElement(tweet);
-     $('#tweet-container').prepend(renderedElem);
-   }
-  }
+}
 
 
-  $(document).ready(function(){
-  
+$(document).ready(function () {
+
   const $form = $('.container .new-tweet form');
 
-  const loadtweets = function(){
+  const loadtweets = function () {
     $.getJSON('/tweets')
-    .then((responseData)=>{
-      console.log('the fetching is successful');
-      $('#tweet-container').empty();
-      renderTweets(responseData);
-    })
+      .then((responseData) => {
+        console.log('the fetching is successful');
+        $('#tweet-container').empty();
+        renderTweets(responseData);
+      })
   }
 
   loadtweets();
 
   $form.submit((event) => {
-    console.log("here");
     event.preventDefault();
-    
-    const serialized = $form.serialize();
-    $.post('/tweets', serialized)
-    .then(()=>{
-      console.log('successful!!!')
-        loadtweets();
-    })
+
+    if ($('.new-tweet form textarea').val().length > 140 || $('.new-tweet form textarea').val().length === 0) {
+      alert('Error: your tweet cannot pass validation');
+    }
+    else {
+      const serialized = $form.serialize();
+      $.post('/tweets', serialized)
+        .then(() => {
+          console.log('successfully serizlized and sent to server')
+          loadtweets();
+        })
+    }
   })
 
-  
+
   //  loadtweets();
 
 });
