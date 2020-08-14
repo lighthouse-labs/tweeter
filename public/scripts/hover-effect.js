8c97ea199ed4eb8ab0dd81526eee2759365fb5f7
Jeffrey Shao <shaoyuhao970909@gmail.com>
2020-08-14 00:27:53 +0000

    eslint checked

--- public/scripts/hover-effect.js
+++ public/scripts/hover-effect.js
@@ -1,16 +1,16 @@
-$(document).ready(function(){
+$(document).ready(function() {
   //when mouse is moving over the tweet:
-  $('#tweet-container').on('mouseover','.tweet', function(){
+  $('#tweet-container').on('mouseover','.tweet', function() {
     $(this).addClass('change');
     $(this).css('opacity','100%');
     $(this).css('border-color','black');
     $('#tweet-container article header .handle').css('display','inline');
-  })
+  });
   //when mouse is moved away from the tweet:
-  $('#tweet-container').on('mouseleave','.tweet', function(){
+  $('#tweet-container').on('mouseleave','.tweet', function() {
     $(this).removeClass('change');
     $(this).css('opacity','70%');
     $(this).css('border-color','rgb(104, 101, 101)');
     $('#tweet-container article header .handle').css('display','none');
-  })
-})
+  });
+});
