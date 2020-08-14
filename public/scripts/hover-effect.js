6e3bd328a788999635706aac63f8850afde5b260
Jeffrey Shao <shaoyuhao970909@gmail.com>
2020-08-14 00:08:17 +0000

    added teo comments

--- public/scripts/hover-effect.js
+++ public/scripts/hover-effect.js
@@ -1,10 +1,12 @@
 $(document).ready(function(){
+  //when mouse is moving over the tweet:
   $('#tweet-container').on('mouseover','.tweet', function(){
     $(this).addClass('change');
     $(this).css('opacity','100%');
     $(this).css('border-color','black');
     $('#tweet-container article header .handle').css('display','inline');
   })
+  //when mouse is moved away from the tweet:
   $('#tweet-container').on('mouseleave','.tweet', function(){
     $(this).removeClass('change');
     $(this).css('opacity','70%');
