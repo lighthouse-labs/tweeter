b5ef7763d0c2842115792fea1e64c0ef9c892a6e
Jeffrey Shao <shaoyuhao970909@gmail.com>
2020-08-14 00:19:05 +0000

    eslint checked

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,19 +1,19 @@
-$(document).ready(function () {
+$(document).ready(function() {
   // when words is typed into textarea:
-  $('.new-tweet form textarea').on('keyup', function (e) {
+  $('.new-tweet form textarea').on('keyup', function(e) {
   //check if numbers of words exceed the limit:
     ($(this).val().length > 140) ?
       $('.new-tweet div .counter').css('color', 'red') : $('.new-tweet div .counter').css('color', '#545149');
-  // if delete key is pressed when textrea is empty: the default of 140 does not change:
+    // if delete key is pressed when textrea is empty: the default of 140 does not change:
     if ($('.new-tweet div .counter').val() === '140' && e.which === 8) {
       $('.new-tweet div .counter').val('140');
       return;
     }
-  //for each key pressed into textarea, default value of 140 decreases, when delete is pressed, the value increases:
+    //for each key pressed into textarea, default value of 140 decreases, when delete is pressed, the value increases:
     if (e.which === 8) {
       $('.new-tweet div .counter').val(parseInt($('.new-tweet div .counter').val()) + 1);
     } else {
       $('.new-tweet div .counter').val(parseInt($('.new-tweet div .counter').val()) - 1);
     }
-  })
-})
+  });
+});
