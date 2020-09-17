22819ce7f3a0bfa1344eee6dbb1bfe37bf9abbec
cplpearce <clint.pearce@rocketmail.com>
2020-09-17 18:45:50 -0400

    Fix JSON data read, add dynamic range to date modifications

--- server/lib/date-adjust.js
+++ server/lib/date-adjust.js
@@ -1,17 +1,17 @@
 "use strict";
 
+// Require fs to write to initial-tweets.json
 const fs = require("fs");
+// Requiring a JSON file automatically parses it and returns the data.
+const tweetsJSON = require('../data-files/initial-tweets.json');
 
 // Write recent dates to initial-tweets.json
 // Specifically sync to not interfere with student functions
 module.exports = () => {
-  // Path to the initial tweets
-  const tweetsPath = "./server/data-files/initial-tweets.json";
-  // Read the tweets synchronously
-  const tweetsRead = JSON.parse(fs.readFileSync(tweetsPath, { encoding: "utf8" }));
   // Iterate tweets and convert the previous time to now()...
-  // And subtract one day (1000ms * 60sec * 60min * 24hrs) * (2 - index)
-  tweetsRead.map((tweet, index) => tweet.created_at = Date.now() - (1000 * 60 * 60 * 24 * (2 - index)));
+  // Then subtract one day (1000ms * 60sec * 60min * 24hrs) * (tweets.length - index)
+  // This keeps the newest tweets at the bottom, and allows for further tweets to be added.
+  tweetsJSON.map((tweet, index) => tweet.created_at = Date.now() - (1000 * 60 * 60 * 24 * (tweetsJSON.length - index)));
   // Re-write the tweets with the new date values.
-  fs.writeFileSync(tweetsPath, JSON.stringify(tweetsRead, null, 2), { encoding: "utf8" });
+  fs.writeFileSync('server/data-files/initial-tweets.json', JSON.stringify(tweetsJSON, null, 2), { encoding: "utf8" });
 };
