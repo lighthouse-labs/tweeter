9a5e9bf10303e4f2b4f607812154b181895407f3
cplpearce <clint.pearce@rocketmail.com>
2020-09-25 20:02:55 -0400

    Change tweetsJSON to a let declaration, and declare it so it's more clear the orig array is being mutated

--- server/lib/date-adjust.js
+++ server/lib/date-adjust.js
@@ -3,7 +3,7 @@
 // Require fs to write to initial-tweets.json
 const fs = require("fs");
 // Requiring a JSON file automatically parses it and returns the data.
-const tweetsJSON = require('../data-files/initial-tweets.json');
+let tweetsJSON = require('../data-files/initial-tweets.json');
 
 // Write recent dates to initial-tweets.json
 // Specifically sync to not interfere with student functions
@@ -13,7 +13,7 @@ module.exports = () => {
   const oneDayMs = 1000 * 60 * 60 * 24
   // Subtract one day in milliseconds (oneDayMs) times the tweets length minus the current index.
   // This keeps the newest tweets at the bottom, and allows for further tweets to be added.
-  tweetsJSON.map((tweet, index) => tweet.created_at = Date.now() - (oneDayMs * (tweetsJSON.length - index)));
+  tweetsJSON = tweetsJSON.map((tweet, index) => tweet.created_at = Date.now() - (oneDayMs * (tweetsJSON.length - index)));
   // Re-write the tweets with the new date values.
   fs.writeFileSync('server/data-files/initial-tweets.json', JSON.stringify(tweetsJSON, null, 2), { encoding: "utf8" });
 };
