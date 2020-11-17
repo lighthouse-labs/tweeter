8fbdee02190b4cae3d69e753879d49fee5590862
Laura Tannahill <lntannahill@gmail.com>
2020-11-17 20:46:35 +0000

    Created event to listen on keyup in text area and updated css for counter to display in red when user out of chars

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,3 +1,10 @@
-$( document ).ready(function() {
-  console.log( "ready!" );
+$("#tweet-text").keyup(function() {
+  const text = 140 - $(this).val().length;
+  const counter = $(this).parents().find(".counter");
+
+  $(counter).val(text);
+  
+  if (text < 0) {
+    $(counter).css("color", "red");
+  }
 });
