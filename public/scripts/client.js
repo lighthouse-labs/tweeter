e354de54d56700f92ef231a53a3824e6d1052789
Laura Tannahill <lntannahill@gmail.com>
2020-11-18 15:19:29 +0000

    Added renderTweets function to append each new tweet to all-tweets

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -5,8 +5,43 @@
  */
 
 $(document).ready(function() {
+
+  // Fake data taken from initial-tweets.json
+  const data = [
+    {
+      "user": {
+        "name": "Newton",
+        "avatars": "https://i.imgur.com/73hZDYK.png"
+        ,
+        "handle": "@SirIsaac"
+      },
+      "content": {
+        "text": "If I have seen further it is by standing on the shoulders of giants"
+      },
+      "created_at": 1461116232227
+    },
+    {
+      "user": {
+        "name": "Descartes",
+        "avatars": "https://i.imgur.com/nlhLi3I.png",
+        "handle": "@rd" },
+      "content": {
+        "text": "Je pense , donc je suis"
+      },
+      "created_at": 1461113959088
+    }
+  ]
+
+  // Render tweet
+  const renderTweets = function(tweets) {
+    const $addTweet = 
+    `${tweets.map(tweet => createTweetElement(tweet)).join("")}`;
+    $('.all-tweets').append($addTweet);
+  }
+
+  // Create tweet
   createTweetElement = function(tweet) {
-    const newTweet = 
+    let newTweet = 
     `<article>
       <header>
         <div class="tweet-user">
@@ -40,23 +75,7 @@ $(document).ready(function() {
   
     return newTweet;
   }
+
+  renderTweets(data);
   
-  // Test / driver code (temporary). Eventually will get this from the server.
-  const tweetData = {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png",
-        "handle": "@SirIsaac"
-      },
-    "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-    "created_at": 1461116232227
-  }
-  
-  const $tweet = createTweetElement(tweetData);
-  
-  // Test / driver code (temporary)
-  console.log($tweet); // to see what it looks like
-  $('.all-tweets').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
 });        
