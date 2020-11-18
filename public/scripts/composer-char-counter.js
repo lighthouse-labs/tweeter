3e323f28fa3278759906f5242d66ae14064d3f5e
Laura Tannahill <lntannahill@gmail.com>
2020-11-18 14:32:37 +0000

    Put DOM code within document.ready

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,10 +1,12 @@
-$("#tweet-text").keyup(function() {
-  const text = 140 - $(this).val().length;
-  const counter = $(this).parents().find(".counter");
-
-  $(counter).val(text);
+$(document).ready(function() {
+  $("#tweet-text").keyup(function() {
+    const text = 140 - $(this).val().length;
+    const counter = $(this).parents().find(".counter");
   
-  if (text < 0) {
-    $(counter).css("color", "red");
-  }
+    $(counter).val(text);
+    
+    if (text < 0) {
+      $(counter).css("color", "red");
+    }
+  });
 });
