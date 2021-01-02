568da8eb0948135a766e1cb3c782e553e1a43c75
Yu Hao Shao <yuhaoshao@YudeMacBook-Air.local>
2021-01-01 20:11:03 -0800

    added port variables

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@
 
 // Basic express setup:
 
-const PORT          = 8080;
+const PORT          = process.env.PORT || 8080;
 const express       = require("express");
 const bodyParser    = require("body-parser");
 const app           = express();
