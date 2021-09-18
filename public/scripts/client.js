f2f6454d3874a38df3c8e0a626625c17633250d8
Lloyd Worth <lloydrww@gmail.com>
2021-09-18 00:01:18 +0000

    started the dyanmic tweet bit

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,3 +4,27 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+{
+//   "user": {
+//     "name": "Newton",
+//     "avatars": "https://i.imgur.com/73hZDYK.png",
+//       "handle": "@SirIsaac"
+//     },
+//   "content": {
+//       "text": "If I have seen further it is by standing on the shoulders of giants"
+//     },
+//   "created_at": 1461116232227
+// }
+
+$(document).ready(function() {
+
+
+  function createTweetElement(tweet) {
+    const name = tweet.user.name;
+    const avatars = tweet.user.avatars;
+    const handle = .handle;
+    const content = .handle;
+    const created = .handle;
+    
+  }
+});
