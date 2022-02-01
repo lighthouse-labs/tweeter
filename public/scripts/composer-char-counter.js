d333445ec2bdce06f5ba1ebf5dc2142e0c9e6ffe
James Phan <tjphan@Jamess-MacBook-Pro.local>
2022-02-01 15:16:48 -0800

    updated character counter to show character limit and red color when negative

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,17 @@
+// jquery for document.ready()
+$(() => {
+
+  // counts number of chars in form
+  const $textarea = $('#tweet-text');
+  $textarea.on('keydown', () => {
+    const $count = $('.counter');
+    $count.text(140 - $textarea.val().length);
+
+    // changes counter to red when negative
+    $textarea.val().length > 140 
+    ? $count.addClass("red")
+    : $count.removeClass("red");
+
+  });
+
+});
