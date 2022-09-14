fdcbea524c4fc0959de76d9e6d546c2d0bc05056
originallykevin <ly1@ualberta.ca>
2022-09-14 23:16:45 +0000

    add event listener and post request

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -6,43 +6,23 @@
 $(document).ready(() => {
   console.log('test');
 
-  const data = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png"
-        ,
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd"
-      },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    }
-
-  ];
-
-  const renderTweets = function(tweets) {
-    // loops through tweets
-    for (const tweet of tweets) {
-      // calls createTweetElement for each tweet
-      const newTweet = createTweetElement(tweet);
-      // takes return value and appends it to the tweets container
-      $('#tweets-container').prepend(newTweet);
-    }
-
+  // move ajax into function
+  const getTweet = () => {
+    $.ajax({
+      type: 'GET',
+      url: '/tweets',
+      success: (tweets) => {
+        $('#tweets-container').empty();
+        console.log(tweets);
+        for (const tweet of tweets) {
+          const newTweet = createTweetElement(tweet);
+          $('#tweets-container').prepend(newTweet);
+        }
+      }
+    });
   };
+
+  getTweet();
 
   const createTweetElement = function(tweet) {
     let username = tweet.user.name;
@@ -71,7 +51,21 @@ $(document).ready(() => {
     return $tweet;
   };
 
-  renderTweets(data);
+  //add event listener and prevent default behaviour(refresh)
+  $('#new-tweet').on('submit', function(event) {
+    event.preventDefault();
+
+    // form handling
+    const serializedData = $(this).serialize();
+    console.log(serializedData);
+
+    // submit post request of serializedData
+    $.post('/tweets', serializedData, (response) => {
+      console.log(response);
+    });
+
+    getTweet();
+  });
 
 });
 
