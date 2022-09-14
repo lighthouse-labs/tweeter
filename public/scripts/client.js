818c7e563df408a7c63dd3af04018f7ade0d375c
originallykevin <ly1@ualberta.ca>
2022-09-14 23:58:02 +0000

    refactor ajax get and post

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,40 +4,31 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 $(document).ready(() => {
-  console.log('test');
 
-  // move ajax into function
-  const getTweet = () => {
-    $.ajax({
-      type: 'GET',
-      url: '/tweets',
-      success: (tweets) => {
-        $('#tweets-container').empty();
-        console.log(tweets);
-        for (const tweet of tweets) {
-          const newTweet = createTweetElement(tweet);
-          $('#tweets-container').prepend(newTweet);
-        }
-      }
-    });
+  // refactor 
+  const loadTweets = function() {
+    $.get('/tweets')
+      .then(function(tweets) {
+        renderTweets(tweets);
+      });
   };
 
-  getTweet();
+  loadTweets();
+
 
   const createTweetElement = function(tweet) {
     let username = tweet.user.name;
     let handle = tweet.user.handle;
     let content = tweet.content.text;
 
-    let $tweet = $(`<article class="tweet-container">
-    <header>
-    <div class="username"><i class="fa-solid fa-user-astronaut fa-2xl">
-      </i>${username}</div>
+    let $tweet = $(`
+  <article class="tweet-container">
+  <header>
+    <div class="username">
+      <i class="fa-solid fa-user-astronaut fa-2xl"></i>${username}</div>
     <div class="userhandle">${handle}</div>
   </header>
-  <p>
-    ${content}
-  </p>
+  <p>${content}</p>
   <footer>
     <div class="date-ago">10 days ago</div>
     <div class="tweet-icon">
@@ -46,26 +37,35 @@ $(document).ready(() => {
       <i class="fa-solid fa-heart tweet-icon-single"></i>
     </div>
   </footer>
-  </article>`); /* Your code for creating the tweet element */
-    // ...
+  </article>
+  `);
+
     return $tweet;
   };
 
   //add event listener and prevent default behaviour(refresh)
   $('#new-tweet').on('submit', function(event) {
     event.preventDefault();
-
     // form handling
     const serializedData = $(this).serialize();
-    console.log(serializedData);
-
     // submit post request of serializedData
-    $.post('/tweets', serializedData, (response) => {
-      console.log(response);
-    });
 
-    getTweet();
+    $.post('/tweets', serializedData)
+      .then(() => {
+        loadTweets();
+      });
   });
 
+
+
+  const renderTweets = function(tweets) {
+    // loops through tweets
+    for (const tweet of tweets) {
+      // calls createTweetElement for each tweet
+      const newTweet = createTweetElement(tweet);
+      // takes return value and appends it to the tweets container
+      $('#tweets-container').prepend(newTweet);
+    }
+  };
 });
 
