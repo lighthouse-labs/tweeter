d7d9be920e4608aa1deb83d6b943539b4f6c9274
eerkmen <efeerkmen@gmail.com>
2022-11-29 00:00:00 +0000

    add loop to go through every tweet to rendertweets function in client.js

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -7,6 +7,9 @@
 const renderTweets = (tweets) => {
   $('.tweets-container').empty(); // Initialized tweets-container
   tweets.sort((a, b) => b.created_at - a.created_at); // sort by created_at desc
-  
+  for (const tweet of tweets) {
+    const $tweet = createTweetElement(tweet);
+    $('.tweets-container').append($tweet);
+  }
 };
 
