3f437e2f51d0361ca7d57f18caed346f964faff0
Hadley <sutherlandhadley@gmail.com>
2022-12-08 19:48:48 -0700

    clean up comments

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,49 +1,10 @@
 $(document).ready(function(el) {
-  console.log("document).ready.working");
-
-  /*
-   * Client-side JS logic goes here
-   * jQuery is already loaded
-   * Reminder: Use (and do all your DOM work in) jQuery's document ready function
-   */
-  // Test / driver code (temporary). Eventually will get this from the server.
-  console.log("test");
 
   const escape = function(str) {
     let div = document.createElement("div");
     div.appendChild(document.createTextNode(str));
     return div.innerHTML;
   };
-
-
-  //$('#submittweet').submit(postTweet);
-
-
-  //const tweetData = [
-  //   {
-  //     "user": {
-  //       "name": "Newton",
-  //       "avatars": "https://i.imgur.com/73hZDYK.png"
-  //       ,
-  //       "handle": "@SirIsaac"
-  //     },
-  //     "content": {
-  //       "text": "If I have seen further it is by standing on the shoulders of giants"
-  //     },
-  //     "created_at": 1461116232227
-  //   },
-  //   {
-  //     "user": {
-  //       "name": "Descartes",
-  //       "avatars": "https://i.imgur.com/nlhLi3I.png",
-  //       "handle": "@rd" },
-  //     "content": {
-  //       "text": "Je pense , donc je suis"
-  //     },
-  //     "created_at": 1461113959088
-  //   }
-  // ]
-
 
   const renderTweets = (tweets) => {
     $('#tweets-container').empty();
@@ -83,7 +44,6 @@ $(document).ready(function(el) {
   const loadTweets = function() {
     $.ajax('/tweets', { method: 'GET' })
       .then((tweets) => {
-        console.log("2 ➤", 2);
         renderTweets(tweets);
       });
     $('.errorText').slideUp(400).text('');
@@ -96,7 +56,6 @@ $(document).ready(function(el) {
 
     $('.errorText').slideUp(400).text('');
 
-    console.log("log", $(this).serialize().length);
     if ($(this).serialize().length < 6) {
       return $('.errorBox').text('Your Tweet has no characters').slideDown();
     }
@@ -104,7 +63,6 @@ $(document).ready(function(el) {
       alert('Your Tweet exceeds the maximum characters');
       return $('.errorBox').text('Your Tweet exceeds the maximum characters').slideDown();
     }
-    console.log('tweet submitted');
     $.ajax('/tweets', {
       method: 'POST',
       data: $(this).serialize()
@@ -120,11 +78,5 @@ $(document).ready(function(el) {
 
   $('form.tweetSubmit').on('submit', postTweet);
 });
-
-// const submitTweet = function(event){
-//   event.preventDefault();
-//   console.log('tweet submitted, sending to database');
-
-// }
 
 
