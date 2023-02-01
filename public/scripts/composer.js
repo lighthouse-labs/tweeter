156015f2f200de9697732ecc787ac71183221c6a
So Hyun (Esther) Choi <estherchoi.sh@gmail.com>
2023-02-01 05:39:17 +0000

    Add dynamic second toggle button

--- public/scripts/composer.js
+++ public/scripts/composer.js
@@ -1,3 +1,14 @@
 $(function () {
-  $(document);
+  $(window).on("scroll", function () {
+    const $newTweetButton = $("#new-tweet");
+    const $newTweetButtonBottom = $("#new-tweet-bottom");
+
+    if ($(window).scrollTop() === 0) {
+      $newTweetButtonBottom.addClass("hide");
+      $newTweetButton.removeClass("hide");
+    } else if ($(window).scrollTop() >= 120) {
+      $newTweetButtonBottom.removeClass("hide");
+      $newTweetButton.addClass("hide");
+    }
+  });
 });
